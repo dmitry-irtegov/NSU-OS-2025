788753e1629dbbed2f94b5d899ffda49ff190dbd
Dmitry V. Irtegov <fat@nsu.ru>
2025-08-08 17:20:57 +0700

    Update README.md
    Update .gitignore - added .DS_store for MacOS, .nfs and various forms of core files
    
    Added some test tools
    
    Lab2 Mashkin (#27)
    
    * initial commit
    
    * restored deleted exit
    
    ready lab

---
+++ 24213/a.kutsenko1/lab3/task3.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    FILE *file;
+    
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+    
+    char *filename = argv[1];
+
+    printf("BEFORE SETUID\n");
+    printf("Real UID: %d\n", getuid());
+    printf("Effective UID: %d\n", geteuid());
+    printf("\n");
+
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+    }
+    else {
+        printf("File opened successfully!\n");
+        fclose(file);
+    }
+    printf("\n");
+
+
+    if (seteuid(getuid()) == -1) {
+        perror("Error in setuid");
+        return 1;
+    }
+
+    printf("AFTER SETUID\n");
+    printf("Real UID: %d\n", getuid());
+    printf("Effective UID: %d\n", geteuid());
+    printf("\n");
+
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+    }
+    else {
+        printf("File opened successfully!\n");
+        fclose(file);
+    }
+    return 0;
+}
+
