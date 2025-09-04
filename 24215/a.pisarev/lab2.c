6219fdce2bfa2ece1adb0e3b7c93c3e0ff1f8b5f
Artem351 <191974021+Artem351@users.noreply.github.com>
2025-09-04 12:19:04 +0700

    lab2

--- 24215/a.pisarev/lab2.c
+++ 24215/a.pisarev/lab2.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <time.h>
+#include <stdlib.h>
+extern char *tzname[];
+
+int main()
+{
+	putenv("TZ=PST8");
+	time_t now;
+	struct tm *sp;
+	
+	(void)time(&now);
+	
+	printf("%s",ctime(&now));
+	sp = localtime(&now);
+	printf("%d/%d/%02d %d:%02d %s\n",
+		sp->tm_mon+1,sp->tm_mday,
+		sp->tm_year,sp->tm_hour,
+		sp->tm_min,tzname[sp->tm_isdst]);
+	return 0;
+}
