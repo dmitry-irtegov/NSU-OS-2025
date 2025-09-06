157cf1fc1944b068de6eaeb0174d5f2b4ded22ae
Artemius951 <a.kutsenko1@g.nsu.ru>
2025-09-06 19:05:04 +0700

    correct warnings

--- task3.c
+++ task3.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
-int main() {
+
+int main(int argc, char *argv[]) {
     FILE *file;
+    
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+    
+    char *filename = argv[1];
 
     printf("BEFORE SETUID\n");
     printf("Real UID: %d\n", getuid());
     printf("Effective UID: %d\n", geteuid());
     printf("\n");
 
-    file = fopen("test3.txt", "w");
+    file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening file");
     }
     else {
-        printf("File opened!\n");
+        printf("File opened successfully!\n");
         fclose(file);
     }
     printf("\n");
+
 
     if (setuid(getuid()) == -1) {
         perror("Error in setuid");
@@ -29,12 +38,12 @@ int main() {
     printf("Effective UID: %d\n", geteuid());
     printf("\n");
 
-    file = fopen("test3.txt", "w");
+    file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening file");
     }
     else {
-        printf("File opened!");
+        printf("File opened successfully!\n");
         fclose(file);
     }
 
