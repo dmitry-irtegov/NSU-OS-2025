57dbdfaf905d95a522d9dee88f8f49d7bf213e2c
Daniel Smolin <danielsmolin5@gmail.com>
2025-09-07 14:19:09 +0700

    sem2_lab1

---
+++ 24215/d.smolin/sem2_lab1/prog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* child_thread(void* arg) {
+    for(int i = 0; i < 10; i++) {
+        printf("Новая нить: строка %d\n", i + 1);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    pthread_create(&thread, NULL, child_thread, NULL);
+    for(int i = 0; i < 10; i++) {
+        printf("Родительская нить: строка %d\n", i + 1);
+    }
+    pthread_join(thread, NULL);
+    return 0;
+}
