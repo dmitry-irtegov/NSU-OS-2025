b6791b90439a98d26b50a39f0ea5124b86eeaa44
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2025-09-08 19:09:23 +0700

    lab3

---
+++ 24216/e.dashkovskii1/lab3/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+
+    const char *filename = "file.txt";
+    FILE *file;
+
+    printf("Real UID: %d, Effective UID: %d\n", getuid(), geteuid());
+
+    file = fopen(filename, "r+");
+    if (!file) {
+        perror("fopen");
+    } else {
+        fclose(file);
+    }
+
+    if (setuid(getuid()) != 0) {
+        perror("setuid");
+        return 1;
+    }
+
+    printf("Real UID: %d, Effective UID: %d\n", getuid(), geteuid());
+
+    file = fopen(filename, "r+");
+    if (!file) {
+        perror("fopen");
+    } else {
+        fclose(file);
+    }
+
+    return 0;
+}
