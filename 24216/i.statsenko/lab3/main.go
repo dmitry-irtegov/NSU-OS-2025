401a6d186ea8104d7e7e987c9160b3eda604e234
Ilya Statsenko <i.statsenko@g.nsu.ru>
2025-09-10 16:24:06 +0700

    добавил решение задачи

---
+++ 24216/i.statsenko/lab3/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+const fileName string = "file.txt"
+
+func TryToOpenFile(name string) {
+	file, err := os.Open(name)
+	if err != nil {
+		fmt.Println(err)
+	}
+	file.Close()
+}
+
+func main() {
+	fmt.Println("RUID: ", syscall.Getuid(), "EUID: ", syscall.Geteuid())
+	TryToOpenFile(fileName)
+
+	err := syscall.Setuid(syscall.Getuid())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("RUID: ", syscall.Getuid(), "EUID: ", syscall.Geteuid())
+	TryToOpenFile(fileName)
+}
