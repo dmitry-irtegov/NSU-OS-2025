9e9b625b73e55095570787d606aa20e6ba508ab9
Andrey Eliseev <a.eliseev2@g.nsu.ru>
2025-09-10 22:29:43 +0700

    Lab9 Eliseev

---
+++ 24213/a.eliseev2/lab9/fork_wait.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <wait.h>
+#include <siginfo.h>
+
+int main() {
+    pid_t result = fork();
+
+    if (result < 0) {
+        perror("fork failed");
+        return 1;
+    } else if (result == 0) {
+        char *const cat_args[] = {"cat", "/usr/share/man/man1/make.1", NULL};
+        if (execvp(cat_args[0], cat_args) == -1) {
+            perror("execv failed");
+            return 1;
+        }
+    }
+
+    printf("parent: Child launched!\n");
+
+    siginfo_t info;
+    if (waitid(P_PID, result, &info, WEXITED)) {
+        perror("waitid failed");
+        return 1;
+    }
+
+    printf("parent: Child exited!\n");
+
+    return 0;
+}
