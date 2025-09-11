cfb17098e0c6131330ed1007d42f6078da816558
Максим Вылегжанин <m.vylegzhanin@g.nsu.ru>
2025-09-11 11:17:27 +0700

    modified

--- 24216/m.vylegzhanin/lab2/ex_time.c
+++ 24216/m.vylegzhanin/lab2/ex_time.c
@@ -2,20 +2,16 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-extern char *tzname[];
 
 main()
 {
     time_t now;
-    struct tm *sp;
-    (void) time( &now );
-    setenv("TZ", "PST8PDT", 1);
-    printf("%s", ctime( &now ) );
-
-    // sp = localtime(&now);
-    // printf("%d/%d/%02d %d:%02d %s\n",
-    //     sp->tm_mon + 1, sp->tm_mday,
-    //     sp->tm_year, sp->tm_hour,
-    //     sp->tm_min, tzname[sp->tm_isdst]);
+    time(&now);
+    if (setenv("TZ", "America/Los_Angeles", 1) == -1){
+        perror("failed to set environment variable");
+        exit(0);
+    }
+    tzset(); 
+    printf("%s", ctime(&now));
     exit(0);
 }
