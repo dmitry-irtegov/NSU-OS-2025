a4071b6a0e080f53ba07484708055ea078bb2686
Anisimov Vladimir <v.anisimov1@g.nsu.ru>
2025-09-16 23:30:25 +0700

    Correct lab4

--- 24214/v.anisimov1/lab4/lab4.c
+++ 24214/v.anisimov1/lab4/lab4.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h> 
 #include <stdlib.h> 
-#define MAX_SIZE 102
+#define BUFF_SIZE 102
+#define MAX_SIZE 100 
 
 typedef struct ListNode
 {
@@ -15,7 +16,9 @@ void PrintStrings (ListNode* head)
 	ListNode* node = head; 
 	while (node != NULL)
 	{
-		printf("String %d: %s\n", i, node->val);
+		printf("String %d: ", i);
+		fputs(node->val, stdout); 
+		printf("\n"); 
 		node = node->next;
 		i += 1;  
 	}
@@ -35,20 +38,22 @@ void ClearList (ListNode* head)
 
 int main()
 {
-	char* buffer = (char*) malloc (sizeof(char) * MAX_SIZE); 
-	fgets(buffer, MAX_SIZE - 1, stdin); 
+	char* buffer = (char*) malloc (sizeof(char) * BUFF_SIZE); 
+	fgets(buffer, BUFF_SIZE, stdin); 
 	ListNode* head = NULL; 
 	ListNode* last_node; 
 	ListNode* curr_node; 
-	while (buffer[0] != '.') // check if input string is not finish signal 
+	while (buffer[0] != '.')
 	{
-		buffer[strlen(buffer) - 1] = '\0'; // set '\n' to '\0' 
+		buffer[MAX_SIZE] = '\0';
+		if (buffer[strnlen(buffer, MAX_SIZE) - 1] == '\n')
+			buffer[strnlen(buffer, MAX_SIZE) - 1] = '\0'; // set '\n' to '\0' 
 		if (buffer[0] == '\0')
 			strcpy(buffer, "**EMPTY STRING**"); 
 		if (head == NULL) 
 		{
 			head = (ListNode*) malloc (sizeof(ListNode)); 
-			head->val = (char*) malloc (strlen(buffer) + 1); 
+			head->val = (char*) malloc (strnlen(buffer, MAX_SIZE) + 1); 
 			strcpy(head->val, buffer); 
 			head->next = NULL; 
 			last_node = head; 
@@ -57,7 +62,7 @@ int main()
 		{
 			curr_node = (ListNode*) malloc (sizeof(ListNode)); 
 			last_node->next = curr_node; 
-			curr_node->val = (char*) malloc (strlen(buffer) + 1);
+			curr_node->val = (char*) malloc (strnlen(buffer, MAX_SIZE) + 1);
 			strcpy(curr_node->val, buffer); 
 			curr_node->next = NULL; 
 			last_node = curr_node; 
@@ -71,5 +76,5 @@ int main()
 	PrintStrings(head); 
 	printf("####\n"); 
 	ClearList(head); 
-	printf("\n...Memory has been cleaned..."); 
+	printf("\n...Memory has been cleaned...\n"); 
 }
