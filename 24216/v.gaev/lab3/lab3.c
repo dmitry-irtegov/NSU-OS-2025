3b0fb395eea0e09640f005089d960ba04ba3abe5
Vladislav Gaev <v.gaev@g.nsu.ru>
2025-09-18 09:57:45 +0700

    test

--- 24216/v.gaev/lab3/lab3.c
+++ 24216/v.gaev/lab3/lab3.c
@@ -7,7 +7,7 @@ int main() {
     // Печатаем реальный и эффективный идентификаторы пользователя
     printf("Real UID: %d\n", getuid());
     printf("Effective UID: %d\n", geteuid());
-
+    char try_open = 0; 
     // Открываем файл для проверки доступа
     FILE *file1 = fopen("file", "r");
     if (file1 == NULL) {
@@ -28,13 +28,15 @@ int main() {
     // Открываем файл для проверки доступа
     FILE *file2 = fopen("file", "r");
     if (file2 == NULL) {
+        try_open = 1;
         perror("Error opening file");
     }
 
     // Закрываем файл, если он был успешно открыт
     fclose(file1);
     fclose(file2);
-    printf("File opened and closed successfully.\n");
-
+    if (try_open == 0){
+        printf("File opened and closed successfully.\n");
+    }
     return 0;
 }
