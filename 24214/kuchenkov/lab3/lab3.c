b0fab955450fd2d79c531636358971259524c7fa
Stepan Kuchenkov <s.kuchenkov@g.nsu.ru>
2025-09-19 00:00:17 +0700

    Add some files

---
+++ 24214/kuchenkov/lab3/lab3.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+    
+int main() {
+    FILE *fp; static char response;
+    if((fp = fopen("answer","a+")) == NULL){
+        fprintf(stderr,"Cannot open answer\n");
+        exit(1);
+    }
+    printf("When opening a file,\n");
+    printf("which id is checked?\n");
+    printf(" (a)real (b) effective\n");
+    while( response != 'a' && response != 'b'){
+        printf("Answer(a/b)? ");
+        scanf("%c%*c", &response);
+    }
+    fprintf(fp,"%5ld:%c\n", getuid(), response);
+    fclose(fp);
+}
