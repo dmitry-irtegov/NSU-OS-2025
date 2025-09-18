c17584937aede19bf90beec6803c52ee67fc57ed
Stepan Kuchenkov <s.kuchenkov@g.nsu.ru>
2025-09-19 01:42:49 +0700

    update2 lab3

--- 24214/kuchenkov/lab3/lab3.c
+++ 24214/kuchenkov/lab3/lab3.c
@@ -3,19 +3,25 @@
 #include <unistd.h>
 
 int main() {
-    FILE *fp; static char response;
+    FILE *fp;
     if((fp = fopen("answer","a+")) == NULL){
         fprintf(stderr,"Cannot open answer\n");
+        perror("Error");
         exit(1);
     }
-    printf("When opening a file,\n");
-    printf("which id is checked?\n");
-    printf(" (a)real (b) effective\n");
-    while( response != 'a' && response != 'b'){
-        printf("Answer(a/b)? ");
-        scanf("%c%*c", &response);
-    }
-    printf("%5ld, %5ld\n", getuid(), geteuid());
-    fprintf(fp,"%5ld:%c\n", getuid(), response);
     fclose(fp);
+
+    printf("%5ld, %5ld\n", getuid(), geteuid());
+    setuid(getuid());
+    
+    if((fp = fopen("answer","a+")) == NULL){
+        fprintf(stderr,"Cannot open answer\n");
+        perror("Error");
+        exit(1);
+    }
+    fclose(fp);
+    
+    printf("%5ld, %5ld\n", getuid(), geteuid());
+    
+    return 0;
 }
