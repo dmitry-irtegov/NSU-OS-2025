858549cb5acf78061723b1b4e3b7a00f1b11903d
Sofia Sidorenko <s.sidorenko@g.nsu.ru>
2025-09-21 14:35:38 +0700

    added signing

--- 24215/s.sidorenko/lab2/lab2.c
+++ 24215/s.sidorenko/lab2/lab2.c
@@ -17,6 +17,7 @@ int main()
     printf("%s", ctime( &now ));
 
     sp = localtime(&now);
+    
     printf("%d/%d/%02d %d:%02d %s\n",
         sp->tm_mon + 1, sp->tm_mday,
         sp->tm_year + 1900, sp->tm_hour,
