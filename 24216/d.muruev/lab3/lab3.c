87f651923d23f25cc51f18aaf65ee733ad23f19f
Denis Muruev <d.muruev@g.nsu.ru>
2025-09-21 15:18:15 +0700

    i

---
+++ 24216/d.muruev/lab3/lab3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>   
+#include <sys/types.h>
+
+void print_ids(const char* filename) {
+    FILE* fp;
+
+    print("RUID: %d\n", getuid());
+    print("EUID: %d\n", geteuid());
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("fopen error");
+        return;
+    } else {
+        printf("File opened successfully\n");
+        fclose(fp);
+    }
+}
+
+int main() {
+    char* filename = "data.txt";
+    print_ids(filename);
+
+    uid_t real_uid = getuid();
+    if (setuid(real_uid) == -1) {
+        perror("setuid error");
+        return 1;
+    }
+
+    print_ids(filename);
+    return 0;
+}
