7a5a927f82baa4e6b34c55fa47c39a9f9805b2bc
StepanEfimov <s.efimov@g.nsu.ru>
2025-09-24 23:04:47 +0700

    add test Error

--- 24216/s.efimov/lab2/lab2.c
+++ 24216/s.efimov/lab2/lab2.c
@@ -1,17 +1,15 @@
-#include <sys/types.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 
 int main(){
-    int status = putenv("TZ=America/Los_Angeles");
-    
-    if (status != 0){
+    if (putenv("TZ=America/Los_Angeles") != 0){
         perror("Falling to get current time.\n");
         exit(EXIT_FAILURE);
     }
 
     time_t CaliforniaTime;
+    
     if (time(&CaliforniaTime) == (time_t)(-1)){
         perror("You can't change the TZ variable to find out the time in California.\n");
         exit(EXIT_FAILURE);
@@ -19,6 +17,10 @@ int main(){
 
     char* now = ctime(&CaliforniaTime);
 
+    if (now == NULL){
+        perror("Fail ctime.\n");
+        exit(EXIT_FAILURE);
+    }
     printf("Current Date and Time: %s\n", now);
     
     exit(EXIT_SUCCESS);
