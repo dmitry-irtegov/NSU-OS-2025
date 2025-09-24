79b343d7f07a0aaf4efe0bb434e5baaaf109d64b
Yuriy Pivkin <y.pivkin@g.nsu.ru>
2025-09-25 00:33:43 +0700

    Решение задачи 2

--- 24215/y.pivkin/lab2/ex_time.c
+++ 24215/y.pivkin/lab2/ex_time.c
@@ -2,33 +2,27 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+extern char *tzname[];
 
-main()
-{
+int main(){
+    setenv("TZ", "US/Pacific", 1);
+    tzset();
+
     time_t now;
-    struct tm *utc;
-    struct tm pst;
+    struct tm *pst;
 
     (void)time(&now);
 
-    printf("Local time:\n");
     printf("%s", ctime(&now));
 
-    utc = gmtime(&now); //UTC time
-    pst = *utc;
+    pst = localtime(&now);
+    printf("%d/%d/%02d %d:%02d %s\n",
+        pst->tm_mon + 1,
+        pst->tm_mday,
+       (pst->tm_year + 1900) % 100,
+        pst->tm_hour,
+        pst->tm_min,
+        tzname[pst->tm_isdst]);
 
-    pst.tm_hour -= 8;
-    pst.tm_isdst = 0;
-
-    mktime(&pst);
-
-    //PST time
-    printf("\nPST time:\n");
-    printf("%s", asctime(&pst));
-    printf("%d/%d/%02d %d:%02d\n",
-        pst.tm_mon + 1,
-        pst.tm_mday,
-       (pst.tm_year + 1900) % 100,
-        pst.tm_hour,
-        pst.tm_min);
+    return 0;
 }
