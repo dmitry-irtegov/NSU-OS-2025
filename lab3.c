0fda0dc997e35b163cb81c3e969c09fc21ea44b8
Artem <191974021+Artem351@users.noreply.github.com>
2025-09-25 10:38:08 +0700

    Add files via upload

---
+++ lab3.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+	 uid_t uid = getuid();
+	 uid_t euid = geteuid();
+	 fprintf(stdout, "your uid:%d\nyour euid:%d\n", uid,euid);
+	FILE *fd;
+	if((fd = fopen("secret.txt","r")) == NULL){
+		perror("fopen1");
+	 }
+	 else{
+		fclose(fd);
+	}
+	
+	euid = geteuid();
+	if(setuid(euid)==-1){
+		perror("setuid\n");
+	}
+	
+	 uid = getuid();
+	 
+	 fprintf(stdout, "your uid:%d\nyour euid:%d\n", uid,euid);
+	if((fd = fopen("secret.txt","r")) == NULL){
+		perror("fopen2");
+	 }
+	 else{
+	 	fclose(fd);
+	 }
+	 
+}
