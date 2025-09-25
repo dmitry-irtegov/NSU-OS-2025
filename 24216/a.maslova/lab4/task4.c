837e236b5bd6d3aa951600206d9693ecdafdd7c3
Alina Maslova <a.maslova1@g.nsu.ru>
2025-09-25 12:05:26 +0700

    change lab4

--- 24216/a.maslova/lab4/task4.c
+++ 24216/a.maslova/lab4/task4.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define BUFFER_SIZE 1024
 
 typedef struct Node {
     char* data;
@@ -58,29 +57,53 @@ void free_list(Node* head) {
     }
 }
 
+char* read_line() {
+    char* line = NULL;
+    size_t size = 0;
+    int c;
+    size_t len = 0;
+
+    while ((c = getchar()) != '\n') {
+        if (len >= size) {
+            size = size == 0 ? 16 : size * 2;
+            line = realloc(line, size);
+            if (!line) {
+                perror("Allocation failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+        line[len++] = c;
+    }
+
+    if (line == NULL) {
+        return NULL;
+    }
+
+    line = realloc(line, len + 1);
+    line[len] = '\0';
+    return line;
+}
+
 int main() {
-    char buffer[BUFFER_SIZE];
     Node* head = NULL;
     Node* tail = NULL;
 
     while(1) {
-        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
-            break;
-        }
-        if (buffer[0] == '.') {
+        char* line = read_line();
+        if (line == NULL) { 
             break;
         }
 
-        size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
-            buffer[len - 1] = '\0';
-            len--;
+        if (line[0] == '.') {
+            free(line);
+            break;
         }
-        
-        Node* newNode = create_node(buffer);
+
+        Node* newNode = create_node(line);
         append_node(&head, &tail, newNode);
+        free(line);
     }
-    
+
     print_list(head);
     free_list(head);
 
