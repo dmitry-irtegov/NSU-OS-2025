5edaf3f350732c0a4d9bbb0f49bcbe1c52d7fac2
StepanEfimov <s.efimov@g.nsu.ru>
2025-09-28 01:47:56 +0700

    add lab3

---
+++ 24216/s.efimov/lab3/lab3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+void check_file(const char* filename){
+    FILE* file;
+    if ((file = fopen(filename, "r")) == NULL) {
+        perror("Не получилось открыть файл");
+        exit(EXIT_FAILURE);  
+    }
+    printf("File opened.\n");
+    fclose(file);
+}
+
+void print_uid(void) {
+    uid_t realUID = getuid();
+    uid_t effectiveUID = geteuid();
+    printf("Real UID: %d\n", realUID);
+    printf("Effective UID: %d\n", effectiveUID);
+}
+
+int main(){
+    print_uid();
+    
+    char* filename = "1.txt";  
+    check_file(filename);
+    
+    if (setuid(getuid()) != 0) {
+        perror("Error sets the effective user ID");
+        return 1;
+    }
+
+    print_uid();
+    
+    check_file(filename);
+
+    exit(EXIT_SUCCESS); // Более явно, чем exit(EXIT_SUCCESS)
+}
