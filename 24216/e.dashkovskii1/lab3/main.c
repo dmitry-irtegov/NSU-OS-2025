f2031212824dd8ea3f857b8226540c09eab48e59
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2025-10-02 11:55:21 +0700

    fix

--- 24216/e.dashkovskii1/lab3/main.c
+++ 24216/e.dashkovskii1/lab3/main.c
@@ -3,18 +3,18 @@
 #include <stdlib.h>
 
 void try_open(const char *filename) {
+    printf("Real UID: %d, Effective UID: %d\n", getuid(), geteuid());
     FILE *file = fopen(filename, "r+");
     if (!file) {
         perror("fopen");
-    } else {
-        fclose(file);
+        return;
     }
+    fclose(file);
 }
 
 int main() {
     const char *filename = "file.txt";
 
-    printf("Real UID: %d, Effective UID: %d\n", getuid(), geteuid());
     try_open(filename);
 
     if (setuid(getuid()) != 0) {
@@ -22,7 +22,6 @@ int main() {
         exit(1);
     }
 
-    printf("Real UID: %d, Effective UID: %d\n", getuid(), geteuid());
     try_open(filename);
 
     exit(0);
