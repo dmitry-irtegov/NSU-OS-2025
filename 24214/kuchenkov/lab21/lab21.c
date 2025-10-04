9e8eb77b8bac4402f63ccfe72a50d0ec5043bb5f
Stepan Kuchenkov <s.kuchenkov@g.nsu.ru>
2025-10-04 12:15:12 +0700

    Stupid bug removed

--- 24214/kuchenkov/lab21/lab21.c
+++ 24214/kuchenkov/lab21/lab21.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <signal.h>
 
@@ -8,12 +9,27 @@ int count = 0;
 void signal_handler(int signal_number) {
     signal(signal_number, signal_handler);
     if (signal_number == SIGINT) {
-        printf("\a");
-        fflush(stdout);
+        write(STDOUT_FILENO, "\a", 1);
         count++;
     } else if (signal_number == SIGQUIT) {
-        printf("%d signals were made", count);
-        exit(0);
+        char buffer[32];
+        const char prefix[] = " signals were made\n";
+        int i = sizeof(buffer) - 1;
+        buffer[i] = '\0';
+
+        int num = count;
+        if (num == 0) {
+            buffer[--i] = '0';
+        } else {
+            while (num > 0 && i > 0) {
+                buffer[--i] = '0' + (num % 10);
+                num /= 10;
+            }
+        }
+        
+        write(STDOUT_FILENO, &buffer[i], strlen(&buffer[i]));
+        write(STDOUT_FILENO, prefix, sizeof(prefix) - 1);
+        _exit(0);
     }
 }
 
