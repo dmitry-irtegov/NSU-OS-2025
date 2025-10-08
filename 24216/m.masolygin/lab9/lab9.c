686ca357cd343d90d72d9c04dd9b93068999e541
Maksim Masolygin <m.masolygin@g.nsu.ru>
2025-10-08 20:44:36 +0700

    fix codestyle

--- 24216/m.masolygin/lab9/lab9.c
+++ 24216/m.masolygin/lab9/lab9.c
@@ -1,55 +1,49 @@
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 // wait - very simple implementation
 // waitid - more more more comlex implementation
 // waitpid - normal complex implementation
 
-int main()
-{
+int main() {
     char *argv[] = {"/bin/cat", "file", (char *)0};
     int status;
     pid_t pid = fork();
 
-    switch (pid)
-    {
-    case 0:
-        execvp("/bin/cat", argv);
-        perror("Error execvp");
-        exit(1);
-
-    case -1:
-        perror("Error fork");
-        exit(1);
-
-    default:
-        printf("Hello from parent!\n");
-
-        pid_t waitpidStatus = waitpid(pid, &status, 0);
-        if (waitpidStatus == -1)
-        {
-            perror("Error waitpid");
+    switch (pid) {
+        case 0:
+            execvp("/bin/cat", argv);
+            perror("Error execvp");
             exit(1);
-        }
 
-        if (WIFEXITED(status))
-        {
-            printf("Child process %d exited with status %d\n", pid, WEXITSTATUS(status));
-        }
-        else if (WIFSIGNALED(status))
-        {
-            printf("Child process %d was terminated by signal %d\n", pid, WTERMSIG(status));
-        }
-        else
-        {
-            printf("Child process %d ended dont normally\n", pid);
-        }
+        case -1:
+            perror("Error fork");
+            exit(1);
 
-        printf("\nChild process %d finished!\n", pid);
-        break;
+        default:
+            printf("Hello from parent!\n");
+
+            pid_t waitpidStatus = waitpid(pid, &status, 0);
+            if (waitpidStatus == -1) {
+                perror("Error waitpid");
+                exit(1);
+            }
+
+            if (WIFEXITED(status)) {
+                printf("Child process %d exited with status %d\n", pid,
+                       WEXITSTATUS(status));
+            } else if (WIFSIGNALED(status)) {
+                printf("Child process %d was terminated by signal %d\n", pid,
+                       WTERMSIG(status));
+            } else {
+                printf("Child process %d ended dont normally\n", pid);
+            }
+
+            printf("\nChild process %d finished!\n", pid);
+            break;
     }
 
     return 0;
