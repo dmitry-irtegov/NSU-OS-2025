fd57d47fc4bc6ae9eb4e6191d38a19ab28a5f843
Maksim Masolygin <m.masolygin@g.nsu.ru>
2025-10-15 09:14:32 +0700

    add return with status

--- 24216/m.masolygin/lab9/lab9.c
+++ 24216/m.masolygin/lab9/lab9.c
@@ -9,13 +9,13 @@
 // waitpid - normal complex implementation
 
 int main() {
-    char *argv[] = {"/bin/cat", "file", (char *)0};
+    char *argv[] = {"cat", "file", (char *)0};
     int status;
     pid_t pid = fork();
 
     switch (pid) {
         case 0:
-            execvp("/bin/cat", argv);
+            execvp("cat", argv);
             perror("Error execvp");
             exit(1);
 
@@ -26,8 +26,7 @@ int main() {
         default:
             printf("Hello from parent!\n");
 
-            pid_t waitpidStatus = waitpid(pid, &status, 0);
-            if (waitpidStatus == -1) {
+            if (waitpid(pid, &status, 0) == -1) {
                 perror("Error waitpid");
                 exit(1);
             }
@@ -35,15 +34,15 @@ int main() {
             if (WIFEXITED(status)) {
                 printf("Child process %d exited with status %d\n", pid,
                        WEXITSTATUS(status));
+                return WEXITSTATUS(status);
             } else if (WIFSIGNALED(status)) {
                 printf("Child process %d was terminated by signal %d\n", pid,
                        WTERMSIG(status));
+                return 128 + WTERMSIG(status);
             } else {
                 printf("Child process %d ended dont normally\n", pid);
+                return 1;
             }
-
-            printf("\nChild process %d finished!\n", pid);
-            break;
     }
 
     return 0;
