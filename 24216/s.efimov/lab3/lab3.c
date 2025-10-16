5623051bf3676e4492ae7eda9752b6d742fb8499
StepanEfimov <s.efimov@g.nsu.ru>
2025-10-16 10:13:15 +0700

    simplification code

--- 24216/s.efimov/lab3/lab3.c
+++ 24216/s.efimov/lab3/lab3.c
@@ -4,34 +4,24 @@
 #include <stdlib.h>
 
 void check_file(const char* filename){
+    printf("Real UID: %d\nEffective UID: %d\n", getuid(), geteuid());
     FILE* file;
     if ((file = fopen(filename, "r")) == NULL) {
-        perror("Не получилось открыть файл");
+        perror("File dont opened");
         return;
     }
     printf("File opened.\n");
     fclose(file);
 }
 
-void print_uid(void) {
-    uid_t realUID = getuid();
-    uid_t effectiveUID = geteuid();
-    printf("Real UID: %d\n", realUID);
-    printf("Effective UID: %d\n", effectiveUID);
-}
-
 int main(){
-    print_uid();
-    
     char* filename = "1.txt";  
     check_file(filename);
     
     if (setuid(getuid()) != 0) {
         perror("Error sets the effective user ID");
-        return 1;
+        exit(EXIT_FAILURE);
     }
-
-    print_uid();
     
     check_file(filename);
 
