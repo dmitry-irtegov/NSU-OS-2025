19d00680fd8c06b66e02b2a1e33d45450ec95304
NetscapeNav <netscapenavigator@yahoo.com>
2025-10-23 23:37:07 +0700

    Revert "to sprintf"
    This reverts commit c570c3913a9aca53217c57d9970202a0fc24175d.

--- 24214/kuchenkov/lab21/lab21.c
+++ 24214/kuchenkov/lab21/lab21.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <signal.h>
 
@@ -8,16 +9,26 @@ int count = 0;
 void signal_handler(int signal_number) {
     signal(signal_number, signal_handler);
     if (signal_number == SIGINT) {
-        if (write(1, "\a", 1) == -1) {
-            _exit(EXIT_FAILURE);
-        }
+        write(STDOUT_FILENO, "\a", 1);
         count++;
     } else if (signal_number == SIGQUIT) {
         char buffer[32];
-        int len = sprintf(buffer, "%d signals were made\n", count);
-        if (write(1, buffer, len) == -1) {
-            _exit(EXIT_FAILURE);
+        const char prefix[] = " signals were made\n";
+        int i = sizeof(buffer) - 1;
+        buffer[i] = '\0';
+
+        int num = count;
+        if (num == 0) {
+            buffer[--i] = '0';
+        } else {
+            while (num > 0 && i > 0) {
+                buffer[--i] = '0' + (num % 10);
+                num /= 10;
+            }
         }
+        
+        write(STDOUT_FILENO, &buffer[i], strlen(&buffer[i]));
+        write(STDOUT_FILENO, prefix, sizeof(prefix) - 1);
         _exit(0);
     }
 }
