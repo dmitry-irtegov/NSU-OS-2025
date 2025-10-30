502dd8963346022852d9fdd7686b81cf0989f1df
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-10-30 10:42:14 +0700

    Create lab2.c (#192)
    принято

---
+++ 24216/a.ermakov/lab2/lab2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(){
+    if (setenv("TZ", "PST8PDT", 1) == -1) {
+        perror("Error setenv:");
+        exit(1);
+    }
+    time_t now;
+    if(time(&now) == -1){
+        perror("Error time:");
+        exit(1);
+    }
+    char* res = ctime(&now);
+    if(res == NULL){
+        perror("Error ctime");
+        exit(1);
+    }
+    printf("Time in California: %s", res);
+    exit(0);
+}
