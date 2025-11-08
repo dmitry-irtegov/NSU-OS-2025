81e94bb83803e2da6a55769e514f9003f9047535
digitalunites <pr.krasnianski@gmail.com>
2025-11-08 15:19:51 +0700

    Fixing signature

--- 24214/p.krasnyanskii/lab16/16.c
+++ 24214/p.krasnyanskii/lab16/16.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
@@ -6,19 +6,52 @@
 char reply(){
     char c;
     struct termios tty, savetty;
-    fflush(stdout);
-    tcgetattr(0, &tty);
+     if (fflush(stdout) == EOF) {
+        perror("fflush");
+        exit(EXIT_FAILURE);
+    }
+
+    if (tcgetattr(STDIN_FILENO, &tty) == -1) {
+        perror("tcgetattr");
+        exit(EXIT_FAILURE);
+    }
+
     savetty = tty;
     tty.c_lflag &= ~(ISIG | ICANON);
     tty.c_cc[VMIN] = 1;
-    tcsetattr(0, TCSANOW, &tty);
-    read(0, &c, 1);
-    tcsetattr(0, TCSANOW, &savetty);
+
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &tty) == -1) {
+        perror("tcsetattr");
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t r = read(STDIN_FILENO, &c, 1);
+    if (r < 0) {
+        perror("read");
+        tcsetattr(STDIN_FILENO, TCSANOW, &savetty);
+        exit(EXIT_FAILURE);
+    }
+
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &savetty) == -1) {
+        perror("tcsetattr (restore settings)");
+        exit(EXIT_FAILURE);
+    }
+
     return c;
 }
 
-void main(){
-    printf("Select any symbol: ");
+int main(void){
+    if (printf("Select any symbol: ") < 0) {
+        perror("printf");
+        exit(EXIT_FAILURE);
+    }
+
     char c = reply();
-    printf("\nYou've selected: %c\n", c);
+
+    if (printf("\nYou've selected: %c\n", c) < 0) {
+        perror("printf");
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
 }
