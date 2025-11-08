1f6d8c44f7c9836aa62a3c8b9d4a75bfb870ff8a
digitalunites <pr.krasnianski@gmail.com>
2025-11-08 09:25:53 +0700

    Error handlers

--- 24214/p.krasnyanskii/lab16/16.c
+++ 24214/p.krasnyanskii/lab16/16.c
@@ -6,21 +6,53 @@
 char reply(){
     char c;
     struct termios tty, savetty;
-    fflush(stdout);
-    tcgetattr(0, &tty);
+     if (fflush(stdout) == EOF) {
+        perror("fflush");
+        exit(EXIT_FAILURE);
+    }
+
+    if (tcgetattr(STDIN_FILENO, &tty) == -1) {
+        perror("tcgetattr");
+        exit(EXIT_FAILURE);
+    }
+
     savetty = tty;
     tty.c_lflag &= ~(ISIG | ICANON);
     tty.c_cc[VMIN] = 1;
-    tcsetattr(0, TCSANOW, &tty);
+
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &tty) == -1) {
+        perror("tcsetattr");
+        exit(EXIT_FAILURE);
+    }
+
     read(0, &c, 1);
-    tcsetattr(0, TCSANOW, &savetty);
+    if (r < 0) {
+        perror("read");
+        tcsetattr(STDIN_FILENO, TCSANOW, &savetty);
+        exit(EXIT_FAILURE);
+    }
+
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &savetty) == -1) {
+        perror("tcsetattr (restore settings)");
+        exit(EXIT_FAILURE);
+    }
+
     return c;
 }
 
 int main(void){
-    printf("Select any symbol: ");
+    if (printf("Select any symbol: ") < 0) {
+        perror("printf");
+        exit(EXIT_FAILURE);
+    }
+
     char c = reply();
-    printf("\nYou've selected: %c\n", c);
-    return 0;
+    
+    if (printf("\nYou've selected: %c\n", c) < 0) {
+        perror("printf");
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
 }
 
