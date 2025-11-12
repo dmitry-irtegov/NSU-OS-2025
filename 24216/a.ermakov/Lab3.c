86cf56bc72babba59c75265be6d38dfe5db73a54
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-11-12 11:37:51 +0700

    сделал чтобы функция открытия файла, возвращала значение -1 в случае ошибки

--- 24216/a.ermakov/Lab3.c
+++ 24216/a.ermakov/Lab3.c
@@ -2,23 +2,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void open_file(const char *filename) {
+int open_file(const char *filename) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
-        perror("Error opening file");
-        exit(1);
+        return -1;
     }
     fclose(file);
+    return 0;
 }
 
 int main() { 
     printf("Real UID: %d, Effective UID: %d\n", getuid(), geteuid());
-    open_file("file");
+    if (open_file("file") == -1) {
+        perror("Error opening file (before setuid)");
+        return 1;
+    }
     if (setuid(getuid()) == -1) {
         perror("Error setting UID");
         exit(1);
     }
     printf("Real UID: %d, Effective UID: %d (after setuid)\n", getuid(), geteuid());
-    open_file("file");
+    if (open_file("file") == -1) {
+        perror("Error opening file (after setuid)");
+        return 1;
+    }
     return 0;
 }
