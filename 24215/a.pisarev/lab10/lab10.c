7407aee9ff3779d65d32b7a8f877177168dce3a9
Artem <191974021+Artem351@users.noreply.github.com>
2025-11-13 09:31:58 +0700

    Create lab10.c

---
+++ 24215/a.pisarev/lab10/lab10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[]){
+	uid_t id = fork();
+	if (id == 0)
+	{
+		execvp(argv[1],&argv[1]);
+		perror("execvp");
+		return 1;
+	}
+
+	
+	int wstatus;
+	if (waitpid(id,&wstatus,0)== -1)
+	{
+		perror("waitpid");
+		return 1;
+	}
+	printf("Exit status=%d.",WEXITSTATUS(wstatus));
+	
+	return 0;
+}
