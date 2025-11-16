80b99f39e0de40b9d9a533c93923c97e19350662
Tokarev Maksim <m.tokarev3@g.nsu.ru>
2025-11-16 17:20:15 +0700

    Fix pipes

--- 24214/m.tokarev3/shell/src/execute.c
+++ 24214/m.tokarev3/shell/src/execute.c
@@ -8,6 +8,12 @@ void execute_commands()
         return;
     }
 
+    if (num_cmds > 1)
+    {
+        execute_pipeline();
+        return;
+    }
+
     pid_t pid, pgid = 0;
     int status;
     char command_line[MAX_LINE];
@@ -31,35 +37,17 @@ void execute_commands()
             tcsetpgrp(shell_terminal, pgid);
         }
 
-        if (bkgrnd)
-        {
-            signal(SIGINT, SIG_DFL);
-            signal(SIGQUIT, SIG_DFL);
-            signal(SIGTSTP, SIG_DFL);
-            signal(SIGTTIN, SIG_DFL);
-            signal(SIGTTOU, SIG_DFL);
-        }
-        else
-        {
-            signal(SIGINT, SIG_DFL);
-            signal(SIGQUIT, SIG_DFL);
-            signal(SIGTSTP, SIG_DFL);
-            signal(SIGTTIN, SIG_DFL);
-            signal(SIGTTOU, SIG_DFL);
-        }
+        signal(SIGINT, SIG_DFL);
+        signal(SIGQUIT, SIG_DFL);
+        signal(SIGTSTP, SIG_DFL);
+        signal(SIGTTIN, SIG_DFL);
+        signal(SIGTTOU, SIG_DFL);
 
         setup_redirections();
 
-        if (num_cmds == 1)
-        {
-            execvp(cmds[0].cmdargs[0], cmds[0].cmdargs);
-            perror(cmds[0].cmdargs[0]);
-            exit(1);
-        }
-        else
-        {
-            execute_pipeline();
-        }
+        execvp(cmds[0].cmdargs[0], cmds[0].cmdargs);
+        perror(cmds[0].cmdargs[0]);
+        exit(1);
     }
     else if (pid > 0) // Родительский процесс
     {
@@ -169,6 +157,12 @@ void execute_pipeline()
                 pgid = pid;
             setpgid(pid, pgid);
 
+            signal(SIGINT, SIG_DFL);
+            signal(SIGQUIT, SIG_DFL);
+            signal(SIGTSTP, SIG_DFL);
+            signal(SIGTTIN, SIG_DFL);
+            signal(SIGTTOU, SIG_DFL);
+
             if (i > 0)
             {
                 dup2(pipes[i - 1][0], STDIN_FILENO);
@@ -207,9 +201,49 @@ void execute_pipeline()
         close(pipes[i][1]);
     }
 
+    // Передаем управление терминалом группе процессов pipeline
+    if (!bkgrnd && shell_is_interactive)
+    {
+        tcsetpgrp(shell_terminal, pgid);
+    }
+
+    char command_line[MAX_LINE];
+    strcpy(command_line, cmds[0].cmdargs[0]);
+    if (bkgrnd)
+    {
+        strcat(command_line, " &");
+    }
+
+    if (bkgrnd)
+    {
+        add_job(pgid, pgid, command_line);
+        printf("[%d] %d\n", get_job_count(), pgid);
+    }
+    else
+    {
+        set_foreground_job(pgid);
+    }
+
     for (int i = 0; i < num_cmds; i++)
     {
         int status;
-        waitpid(pids[i], &status, 0);
+        waitpid(pids[i], &status, WUNTRACED);
+
+        if (WIFSTOPPED(status))
+        {
+            if (!bkgrnd)
+            {
+                add_job(pgid, pgid, command_line);
+                set_job_status(pgid, JOB_STOPPED);
+                printf("[%d] Stopped\t\t%s\n", get_job_count(), command_line);
+            }
+        }
+    }
+
+    // Возвращаем управление терминалом обратно shell
+    if (!bkgrnd && shell_is_interactive)
+    {
+        tcsetpgrp(shell_terminal, shell_pgid);
+        set_foreground_job(0);
     }
 }
