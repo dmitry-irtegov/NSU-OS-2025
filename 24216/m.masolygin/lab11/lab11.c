76b3508bdba056dee3fc0cae74f8fb3714ebf472
Maksim Masolygin <m.masolygin@g.nsu.ru>
2025-11-19 19:34:43 +0700

    add lab11

---
+++ 24216/m.masolygin/lab11/lab11.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+extern char** environ;
+
+int execvpe(const char* file, char* const argv[], char* const envp[]) {
+    char** origin_environ = environ;
+    environ = (char**)envp;
+    execvp(file, argv);
+    environ = origin_environ;
+    return -1;
+};
+
+int main(int argc, char* argv[]) {
+    int status;
+    pid_t pid = fork();
+
+    switch (pid) {
+        case 0:
+            char* argv[] = {"sh", "-c", "echo MY_VAR=$MY_VAR", NULL};
+            char* envp[] = {"MY_VAR=Hello_from_execvpe", "PATH=/bin:/usr/bin",
+                            NULL};
+            execvpe("sh", argv, envp);
+            perror("Error execvp");
+            exit(1);
+
+        case -1:
+            perror("Error fork");
+            exit(1);
+
+        default:
+            if (waitpid(pid, &status, 0) == -1) {
+                perror("Error waitpid");
+                exit(1);
+            }
+
+            if (WIFEXITED(status)) {
+                return WEXITSTATUS(status);
+            } else if (WIFSIGNALED(status)) {
+                return 128 + WTERMSIG(status);
+            } else {
+                return 1;
+            }
+    }
+
+    return 0;
+}
