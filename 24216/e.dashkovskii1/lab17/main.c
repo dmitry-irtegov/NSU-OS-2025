4606f12ba252bad6440acd793cc539e97beb9f37
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2025-11-20 10:51:26 +0700

    fix

--- 24216/e.dashkovskii1/lab17/main.c
+++ 24216/e.dashkovskii1/lab17/main.c
@@ -46,46 +46,54 @@ void process_input() {
     while (1) {
         c = getchar();
 
-        if (c == 4 && len == 0) { //CTRL-D && empty line
-            break;
-        }
-
-        if (isprint(c)) {
-            if (len < MAX_LEN) {
-                line[len++] = c;
-                putchar(c);
-            } else {
-                bell();
-            }
-        } else if (c == 127) {  // ERASE
-            if (len > 0) {
-                len--;
-                printf("\b \b"); 
-            }
-        } else if (c == 23) {  // CTRL-W
-            if (len > 0) {
-                while (len > 0 && isspace(line[len - 1])) {
-                    len--;
-                    printf("\b \b");
+        switch (c) {
+            case 4: // CTRL-D
+                if (len == 0) {
+                    return;  
+                } else {
+                    bell();
                 }
-                while (len > 0 && !isspace(line[len - 1])) {
-                    len--;
-                    printf("\b \b");
-                }
-            }
-        } else if (c == 21) {  // KILL (CTRL-U)
-            while (len > 0) {
-                len--;
-                printf("\b \b");
-            }
-        } else if (c == 4) {  // CTRL-D
-            if (len == 0) {
                 break;
-            } else {
-                bell();
-            }
-        } else {
-            bell();
+
+            case 127: // ERASE (backspace)
+                if (len > 0) {
+                    len--;
+                    printf("\b \b");
+                }
+                break;
+
+            case 23: // CTRL-W
+                if (len > 0) {
+                    while (len > 0 && isspace(line[len - 1])) {
+                        len--;
+                        printf("\b \b");
+                    }
+                    while (len > 0 && !isspace(line[len - 1])) {
+                        len--;
+                        printf("\b \b");
+                    }
+                }
+                break;
+
+            case 21: // CTRL-U (kill)
+                while (len > 0) {
+                    len--;
+                    printf("\b \b");
+                }
+                break;
+
+            default:
+                if (isprint(c)) {
+                    if (len < MAX_LEN) {
+                        line[len++] = c;
+                        putchar(c);
+                    } else {
+                        bell();
+                    }
+                } else {
+                    bell();
+                }
+                break;
         }
     }
 
