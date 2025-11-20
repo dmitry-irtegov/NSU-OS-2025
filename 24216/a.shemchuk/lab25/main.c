75cdb0b0e9ccb471c7119ca80cfe9d180af8372e
ashemchuk <149040192+ashemchuk@users.noreply.github.com>
2025-11-20 11:19:21 +0700

    lab25 (#272)

---
+++ 24216/a.shemchuk/lab25/main.c
@@ -0,0 +1,63 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define BUF_SIZE 256
+
+int main() {
+  int fd[2];
+  if (pipe(fd) == -1) {
+    perror("Couldn't open pipe");
+    return 1;
+  }
+
+  pid_t pid = fork();
+  if (pid == -1) {
+    perror("Fork was failed");
+    return 1;
+  }
+
+  if (pid > 0) {
+    // parent
+    if (close(fd[1])) {
+      perror("Couldn't close end of pipe");
+      return 1;
+    }
+    char buf[BUF_SIZE];
+    ssize_t r;
+    while ((r = read(fd[0], buf, BUF_SIZE)) > 0) {
+      for (size_t i = 0; i < (size_t)r; i++) {
+        buf[i] = (char)toupper(buf[i]);
+      }
+
+      if (fwrite(buf, sizeof(char), (size_t)r, stdout) != (size_t)r) {
+        perror("Failed to print message");
+        return 1;
+      }
+    }
+    if (close(fd[0])) {
+      perror("Couldn't close end of pipe");
+      return 1;
+    }
+    wait(NULL);
+  }
+  else {
+    // child
+    if (close(fd[0])) {
+      perror("Couldn't close end of pipe");
+      return 1;
+    }
+    char msg[] =
+      "Hi! I'm your child and I want you to uppercase this message\n";
+    if (write(fd[1], msg, sizeof(msg)) == -1) {
+      perror("Writing message into pipe was failed");
+      return 1;
+    }
+    if (close(fd[1])) {
+      perror("Couldn't close end of pipe");
+      return 1;
+    }
+  }
+  return 0;
+}
