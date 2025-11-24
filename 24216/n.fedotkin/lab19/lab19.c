01f521b7ff7a02e9efe4aa2c16830bcc8b5a788d
Nikita Fedotkin <n.fedotkin@g.nsu.ru>
2025-11-24 21:16:57 +0700

    lab 19

---
+++ 24216/n.fedotkin/lab19/lab19.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <errno.h>
+
+int match_pattern(const char *pattern, const char *name) {
+    if (*pattern == '\0' && *name == '\0')
+        return 1;
+
+    if (*pattern == '*') {
+        return match_pattern(pattern + 1, name) ||
+               (*name && match_pattern(pattern, name + 1));
+    }
+
+    if (*pattern == '?')
+        return *name && match_pattern(pattern + 1, name + 1);
+
+    if (*pattern == *name)
+        return match_pattern(pattern + 1, name + 1);
+
+    return 0;
+}
+
+int scan_directory(const char *pattern) {
+    DIR *dir = opendir(".");
+    if (!dir) {
+        perror("opendir");
+        exit(EXIT_FAILURE);
+    }
+
+    struct dirent *entry;
+    int found = 0;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (match_pattern(pattern, entry->d_name)) {
+            printf("%s\n", entry->d_name);
+            found = 1;
+        }
+    }
+
+    if (errno) {
+        perror("readdir");
+        closedir(dir);
+        exit(EXIT_FAILURE);
+    }
+
+    closedir(dir);
+    return found;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "To use: %s <template>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (strchr(argv[1], '/')) {
+        fprintf(stderr, "Error: '/' must not occur in the template!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int result = scan_directory(argv[1]);
+
+    if (!result) {
+        printf("%s\n", argv[1]);
+    }
+
+    exit(EXIT_SUCCESS);
+}
