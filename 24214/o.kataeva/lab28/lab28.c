d4cfcaa0529dc59cacb7c4dbdd7a6c722887db33
olesik111 <o.kataeva@g.nsu.ru>
2025-11-24 11:40:33 +0700

    lab28 kataeva

---
+++ 24214/o.kataeva/lab28/lab28.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    FILE *pipes[2];
+    int val;
+    int printed = 0;
+
+    srand((unsigned)time(NULL));
+
+    if (p2open("sort -n", pipes) < 0) {
+        fprintf(stderr, "p2open");
+        return 1;
+    }
+
+    for (int k = 0; k < 100; k++) {
+        fprintf(pipes[0], "%d\n", rand() % 100);
+    }
+    fclose(pipes[0]);
+
+    while (fscanf(pipes[1], "%d", &val) == 1) {
+        printf("%3d ", val);
+        printed++;
+        if (printed % 10 == 0)
+            putchar('\n');
+    }
+    if (printed % 10 != 0)
+        putchar('\n');
+
+    p2close(pipes);
+    return 0;
+}
