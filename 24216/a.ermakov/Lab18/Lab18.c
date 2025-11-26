988b97f1c8c20069a1a61c6b5706d36e54128c27
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-11-26 18:57:10 +0700

    Create Lab18.c

---
+++ 24216/a.ermakov/Lab18/Lab18.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <string.h>
+#include <libgen.h>
+
+char filetype(mode_t mode) {
+    if (S_ISDIR(mode)) return 'd';
+    if (S_ISREG(mode)) return '-';
+    return '?';
+}
+
+void get_perms(mode_t mode, char *buf) {
+    buf[0] = (mode & S_IRUSR) ? 'r' : '-';
+    buf[1] = (mode & S_IWUSR) ? 'w' : '-';
+    buf[2] = (mode & S_IXUSR) ? 'x' : '-';
+
+    buf[3] = (mode & S_IRGRP) ? 'r' : '-';
+    buf[4] = (mode & S_IWGRP) ? 'w' : '-';
+    buf[5] = (mode & S_IXGRP) ? 'x' : '-';
+
+    buf[6] = (mode & S_IROTH) ? 'r' : '-';
+    buf[7] = (mode & S_IWOTH) ? 'w' : '-';
+    buf[8] = (mode & S_IXOTH) ? 'x' : '-';
+
+    buf[9] = '\0';
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s file...\n", argv[0]);
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        struct stat st;
+        if (lstat(argv[i], &st) < 0) {
+            perror(argv[i]);
+            continue;
+        }
+
+        char perms[10];
+        get_perms(st.st_mode, perms);
+
+        struct passwd *pw = getpwuid(st.st_uid);
+        struct group  *gr = getgrgid(st.st_gid);
+
+        char timebuf[64];
+        struct tm *tm = localtime(&st.st_mtime);
+        strftime(timebuf, sizeof(timebuf), "%Y-%m-%d %H:%M", tm);
+
+        char *name = basename(argv[i]);
+
+        printf("%c%s %3lu %-8s %-8s ",
+               filetype(st.st_mode),
+               perms,
+               st.st_nlink,
+               pw ? pw->pw_name : "unknown",
+               gr ? gr->gr_name : "unknown");
+
+        if (S_ISREG(st.st_mode))
+            printf("%8ld ", st.st_size);
+        else
+            printf("         ");  
+
+        printf("%s %s\n", timebuf, name);
+    }
+
+    return 0;
+}
