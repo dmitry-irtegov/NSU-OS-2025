6e6e77dbc788ffa3fbe6eac67d319fe80eebbee7
Anastasia Ryzhneva <a.ryzhneva@g.nsu.ru>
2025-11-27 01:32:56 +0700

    lab26

---
+++ 24216/a.ryzhneva/lab26/lab26.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    FILE *fp;
+    int status;
+    const char *message = "cheCKCK tesTTTTT\n";
+    fp = popen("tr [:lower:] [:upper:]", "w");
+    
+    if (fp == NULL) {
+        perror("popen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fputs(message, fp) == EOF) {
+        perror("fputs failed");
+        pclose(fp);
+        exit(EXIT_FAILURE);
+    }
+    
+    status = pclose(fp);
+
+    if (status == -1) {
+        perror("pclose failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (WIFEXITED(status)) {
+        printf("\nchild process finished sucsess %d\n", WEXITSTATUS(status));
+    } else {
+        printf("\nerror finished child process\n");
+    }
+
+    return 0;
+}
