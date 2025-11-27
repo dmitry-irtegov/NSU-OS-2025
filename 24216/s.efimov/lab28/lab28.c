a984b4262b961ee15f712d127a91141ca60295e0
rend <stepan.efimov2006@mail.ru>
2025-11-27 11:22:30 +0700

    create lab28 (#294)
    accepted

---
+++ 24216/s.efimov/lab28/lab28.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <libgen.h>
+
+int main() {
+    FILE *filePointer[2];
+
+    if (p2open("sort -n", filePointer) == -1) {
+        perror("error p2open");
+        return EXIT_FAILURE;
+    }
+
+    srand(time(NULL));
+
+    for (int i = 0; i < 100; i++) {
+        fprintf(filePointer[0], "%d\n", rand() % 100);
+    }
+
+    if (fclose(filePointer[0]) == -1){
+        perror("failed close filePointer[0]");
+        exit(EXIT_FAILURE);
+    }
+
+    int number,count = 0;
+    while (fscanf(filePointer[1], "%d", &number) != EOF) {
+        printf("%d ", number);
+        count++;
+        if (count % 10 == 0) {
+            printf("\n");
+        }
+    }
+
+    if (p2close(filePointer) == -1) {
+        perror("error p2close");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
