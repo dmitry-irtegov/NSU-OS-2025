6b8ed1e59e2d16cf24a8d0cebe5b725584e014d8
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-11-27 11:15:51 +0700

    Lab 9 (#414)
    accepted

---
+++ 24216/a.ermakov/Lab9/lab9.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void) {
+    pid_t child_pid = fork();
+    pid_t wait_result;
+
+    switch (child_pid) {
+        case 0:
+            execlp("cat", "cat", "test.txt", (char *)NULL);
+            perror("Ошибка запуска программы cat");
+            exit(EXIT_FAILURE);
+
+        case -1:
+            perror("Не удалось создать дочерний процесс");
+            exit(EXIT_FAILURE);
+
+        default:
+            wait_result = waitpid(child_pid, NULL, 0);
+            switch (wait_result) {
+                case -1:
+                    perror("Сбой при ожидании завершения процесса");
+                    exit(EXIT_FAILURE);
+
+                default:
+                    printf("\nЭтап предварительной обработки успешно завершён.\n");
+            }
+    }
+
+    return 0;
+}
