004379500ae9eb762adab837f307bf99022f10d3
Kirpich Mira <k.maltsev@g.nsu.ru>
2025-12-04 09:39:35 +0700

    lab30

--- 24215/k.maltsev/lab30/client.c
+++ 24215/k.maltsev/lab30/client.c
@@ -1,9 +1,11 @@
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <errno.h>
 
 #define SOCKET_PATH "mysocket"
 #define BUFFER_SIZE 1024
--- 24215/k.maltsev/lab30/server.c
+++ 24215/k.maltsev/lab30/server.c
@@ -1,3 +1,4 @@
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,6 +6,7 @@
 #include <ctype.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <errno.h>
 
 #define SOCKET_PATH "mysocket" 
 #define BUFFER_SIZE 1024
