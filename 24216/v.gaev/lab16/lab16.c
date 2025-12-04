5087f6fc3b33c2fe692d232ed0d76202200666ef
Гаев Владислав <90602583+WorstNormal@users.noreply.github.com>
2025-12-04 10:14:50 +0700

    Update lab16.c

--- 24216/v.gaev/lab16/lab16.c
+++ 24216/v.gaev/lab16/lab16.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <termios.h>
+#include <unistd.h>
 
-int main(void) {
+int main() {
     struct termios old_settings, new_settings;
-    char answer;
+    char c;
 
     if (tcgetattr(STDIN_FILENO, &old_settings) != 0) {
-        perror("tcgetattr");
-        exit(EXIT_FAILURE);
+        perror("Ошибка при получении атрибутов");
+        return 1;
     }
 
     new_settings = old_settings;
-    new_settings.c_lflag &= ~ICANON;
+    new_settings.c_lflag &= (~ICANON);
     new_settings.c_cc[VMIN] = 1;
     new_settings.c_cc[VTIME] = 0;
 
     if (tcsetattr(STDIN_FILENO, TCSANOW, &new_settings) != 0) {
-        perror("tcsetattr");
-        exit(EXIT_FAILURE);
+        perror("Ошибка при установке атрибутов");
+        return 1;
     }
 
-    printf("2 + 2 = ?");
+    printf("2 + 2 = ");
     fflush(stdout);
 
-    answer = getchar();
-    printf("\n\nВы нажали: '%c'\n", answer);
+    c = getchar();
+
     tcsetattr(STDIN_FILENO, TCSANOW, &old_settings);
+
+    printf("\n\nВы нажали: '%c'\n", c);
 
     return 0;
 }
