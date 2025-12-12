2e036a7cac5c6559fec48d9bdf83a7033245e7b3
Deshin Mikhail <m.deshin@g.nsu.ru>
2025-12-12 22:39:20 +0700

    added checking exit status of child

--- 24214/m.deshin/lab26/lab26.c
+++ 24214/m.deshin/lab26/lab26.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <sys/wait.h>
 
 int main() {
     FILE *fp = popen("printf '%s' \"I need to pass lab26!\"", "r");
@@ -19,6 +20,14 @@ int main() {
     if (stat == -1) {
         perror("pclose failed");
         return 1;
+    }
+
+    if (WIFEXITED(stat)) {
+        if (WEXITSTATUS(stat) != 0) {
+            fprintf(stderr, "child didn't terminate normally");
+        }
+    } else {
+        fprintf(stderr, "child crashed");
     }
 
     return 0;
