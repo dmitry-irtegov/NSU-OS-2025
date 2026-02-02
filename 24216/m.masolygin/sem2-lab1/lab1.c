84632b7c940d94b0c3e29ad15954546bbd7e9c38
Maksim Masolygin <m.masolygin@g.nsu.ru>
2026-02-02 21:11:31 +0700

    lab1

---
+++ 24216/m.masolygin/sem2-lab1/lab1.c
@@ -0,0 +1,32 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_lines(char* str) {
+    for (int i = 1; i <= 10; i++) {
+        printf("%s %d\n", str, i);
+    }
+}
+
+void* printer_ten(void* arg) {
+    print_lines((char*)arg);
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t thread;
+
+    if (pthread_create(&thread, NULL, printer_ten, "Child string:") != 0) {
+        perror("Error creating thread");
+        return 1;
+    }
+
+    print_lines("Parent string:");
+
+    if (pthread_join(thread, NULL) != 0) {
+        perror("Error joining thread");
+        return 1;
+    }
+
+    return 0;
+}
