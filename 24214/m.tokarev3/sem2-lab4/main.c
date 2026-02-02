4617013d2af03e05a7fb3b23e3ca41aca41b348c
Tokarev Maksim <m.tokarev3@g.nsu.ru>
2026-02-02 22:51:57 +0700

    sem2-lab04 m.tokarev3
    sem2-lab04 m.tokarev3

---
+++ 24214/m.tokarev3/sem2-lab4/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *my_func(void *arg)
+{
+    while (1)
+    {
+        printf("Some text ");
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    pthread_t new_thread;
+
+    if (pthread_create(&new_thread, NULL, my_func, NULL) != 0)
+    {
+        perror("pthread_create");
+        return 1;
+    }
+
+    sleep(2);
+
+    if (pthread_cancel(new_thread) != 0)
+    {
+        perror("pthread_cancel");
+        return 1;
+    }
+
+    return 0;
+}
