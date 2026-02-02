450c019a8b1c46b49ef58df5ef2165c261dee35a
olesik111 <o.kataeva@g.nsu.ru>
2026-02-02 13:03:17 +0700

    lab4 sem2

---
+++ 24214/o.kataeva/sem2-lab4/sem2-lab4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* thread_body(void* param) {
+    (void)param;
+
+    while (1) {
+        write(1, "Child\n", 6);
+    }
+
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    void* res;
+
+    if (pthread_create(&thread, NULL, thread_body, NULL) != 0) {
+        perror("pthread_create");
+        return 1;
+    }
+
+    sleep(2);
+
+    if (pthread_cancel(thread) != 0) {
+        perror("pthread_cancel");
+        return 1;
+    }
+
+    pthread_join(thread, &res);
+
+    if (res == PTHREAD_CANCELED) {
+        printf("\nCancelled\n");
+    }
+
+    return 0;
+}
