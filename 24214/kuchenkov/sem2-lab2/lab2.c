943cd0b2cc201d056790b9f37a4def5c1c48c86c
NetscapeNav <netscapenavigator@yahoo.com>
2026-02-03 08:34:42 +0700

    lab2

---
+++ 24214/kuchenkov/sem2-lab2/lab2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void* ten_strings(void* arg) {
+    for (int i = 0; i < 9; i++) {
+        printf("Ten strings.\n");
+    }
+    printf("Tenth string.\n");
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    int code;
+
+    code = pthread_create(&thread, NULL, ten_strings, NULL);
+    if (code != 0) {
+        fprintf(stderr, "Error creating thread.\n");
+        return 1;
+    }
+
+    code = pthread_join(thread, NULL);
+    if (code != 0) {
+        fprintf(stderr, "Error joining thread.\n");
+        return 1;
+    }
+    for (int i = 0; i < 9; i++) {
+        printf("Ten strings.\n");
+    }
+    printf("Tenth string.\n");
+
+    return 0;
+}
