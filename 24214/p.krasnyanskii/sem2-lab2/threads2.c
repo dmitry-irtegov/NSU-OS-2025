c66311a7e48f216a514722fec09bf4238787ce2c
digitalunites <pr.krasnianski@gmail.com>
2026-02-03 23:35:27 +0700

    Task 2 done

---
+++ 24214/p.krasnyanskii/sem2-lab2/threads2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void* child_thread(void* arg) {
+    for (int i = 0; i < 10; i++) {
+        printf("Child thread: %d\n", i);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t tid;
+
+    if (pthread_create(&tid, NULL, child_thread, NULL) != 0) {
+        perror("pthread_create");
+        return 1;
+    }
+
+    pthread_join(tid, NULL);
+
+    for (int i = 0; i < 10; i++) {
+        printf("Parent thread: %d\n", i);
+    }
+
+    return 0;
+}
