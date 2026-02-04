76bfdda6c6a98031b74cb2d63aa3a2efb8f4b974
Sofia Sidorenko <s.sidorenko@g.nsu.ru>
2026-02-04 13:47:13 +0700

    sem 2 task 1 solution: thread creation

---
+++ 24215/s.sidorenko/sem2_lab1/lab1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+void *foo_thread() {
+    int i = 1;
+    while (i <= 10) {
+        printf("thead line %d\n", i);
+        i++;
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+
+    if (pthread_create(&thread, NULL, foo_thread, NULL) != 0){
+        printf("Error with thread creating");
+        return EXIT_FAILURE;
+    }
+    pthread_join(thread, NULL);
+
+    for (int i = 1; i <= 10; i++) {
+        printf("parent line %d\n", i);
+    }
+
+
+    return 0;
+}
