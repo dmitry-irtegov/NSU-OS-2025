b75f59b826fe1c4656650f7fdf58fcfe078fdcea
Maksim Masolygin <m.masolygin@g.nsu.ru>
2026-02-05 21:51:49 +0700

    fix: remove excess

--- 24216/m.masolygin/sem2-lab2/lab2.c
+++ 24216/m.masolygin/sem2-lab2/lab2.c
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void print_lines(char* str) {
     for (int i = 1; i <= 10; i++) {
@@ -16,13 +17,17 @@ void* printer_ten(void* arg) {
 int main() {
     pthread_t thread;
 
-    if (pthread_create(&thread, NULL, printer_ten, "Child string:") != 0) {
-        perror("Error creating thread");
+    int error = pthread_create(&thread, NULL, printer_ten, "Child string:");
+
+    if (error != 0) {
+        fprintf(stderr, "Error creating threads: %s\n", strerror(error));
         return 1;
     }
 
-    if (pthread_join(thread, NULL) != 0) {
-        perror("Error joining thread");
+    error = pthread_join(thread, NULL);
+
+    if (error != 0) {
+        fprintf(stderr, "Error joining threads: %s\n", strerror(error));
         return 1;
     }
 
