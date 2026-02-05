11d5d64613f225473559fe1babef9e84809902f6
Anastasia Ryzhneva <a.ryzhneva@g.nsu.ru>
2026-02-05 20:26:35 +0700

    sem2-lab1

---
+++ 24216/a.ryzhneva/sem2-lab1/lab1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdlib.h>
+
+void* thread_body(void* param) {
+    for(int i = 0; i < 10; i++) {
+        printf("Child\n");
+    }
+    return NULL;
+}
+int main(int argc, char* argv[]) {
+    pthread_t thread;
+    int code;
+
+    code = pthread_create(&thread, NULL, thread_body, NULL);
+    if (code != 0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof buf);
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+
+    for(int i = 0; i < 10; i++) {
+        printf("Parent\n");
+    }
+
+    pthread_exit(NULL);
+}
