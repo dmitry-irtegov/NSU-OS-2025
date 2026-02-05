b239e21fa374f1bd16529c074f55f8716cc11ba2
Nikita Fedotkin <n.fedotkin@g.nsu.ru>
2026-02-05 20:56:43 +0700

    lab2 sem2

---
+++ 24216/n.fedotkin/sem2-lab2/lab2.c
@@ -0,0 +1,33 @@
+#include "pthread.h"
+#include "stdio.h"
+#include <stdlib.h>
+#include <string.h>
+
+void* thread_printer(void *arg) {
+    for (int i = 0; i < 10; i++) {
+        printf("Child thread: %d\n", i);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    int result = 0;
+
+    result = pthread_create(&thread, NULL, thread_printer, NULL);
+    if (result != 0) {
+        fprintf(stderr, "Error pthread_create: %s\n", strerror(result));
+    }
+
+    result = pthread_join(thread, NULL);
+    if (result != 0) {
+        fprintf(stderr, "Error pthread_join: %s\n", strerror(result));
+    }
+
+
+    for (int i = 0; i < 10; i++) {
+        printf("Main thread: %d\n", i);
+    }
+
+    return 0;
+}
