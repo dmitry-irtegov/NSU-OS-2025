8af48309e9fd2d788c3dfe8938ce2a79bc53acac
Denis Muruev <d.muruev@g.nsu.ru>
2026-02-05 20:54:03 +0700

    init

---
+++ 24216/d.muruev/sem2-lab1/sem2-lab1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+
+void* print_10_lines(void *arg) {
+    for (int i = 0; i < 10; i++) {
+        printf("new thread\n");
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    int result = pthread_create(&thread, NULL, print_10_lines, NULL);
+    if (result != 0) {
+        fprintf(stderr, "Error creating thread: %s\n", strerror(result));
+        exit(1);
+    }
+    for (int i = 0; i < 10; i++) {
+        printf("main thread\n");
+    }
+    pthread_exit(NULL);
+}
