5cb360c663b96b57c67ca934ddcbd1f14761ce98
olesik111 <o.kataeva@g.nsu.ru>
2026-02-05 10:24:12 +0700

    sem2-lab14 kataeva

---
+++ 24214/o.kataeva/sem2-lab14/lab14.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+sem_t sem_child;
+sem_t sem_parent;
+
+void* printer(void* arg) {
+    for (int k = 0; k < 10; k++) {
+        sem_wait(&sem_child);
+        printf("Child thread: step %d\n", k);
+        sem_post(&sem_parent);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    int status;
+
+    sem_init(&sem_child, 0, 1);
+    sem_init(&sem_parent, 0, 0);
+
+    status = pthread_create(&thread, NULL, printer, NULL);
+    if (status != 0) {
+        perror("Error creating thread");
+        exit(1);
+    }
+
+    for (int k = 0; k < 10; k++) {
+        sem_wait(&sem_parent);
+        printf("Main thread:  step %d\n", k);
+        sem_post(&sem_child);
+    }
+
+    pthread_join(thread, NULL);
+
+    sem_destroy(&sem_child);
+    sem_destroy(&sem_parent);
+
+    return 0;
+}
