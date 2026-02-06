90698277ec6b10708d0bf18c9d714be829c27ccf
Anastasiya Ryzhneva <158774415+kettleboiled@users.noreply.github.com>
2026-02-06 10:42:33 +0700

    sem2-lab1 ryzhneva (#586)
    * sem2-lab1
    
    * fix
    
    * fix
    
    * fix stderr

---
+++ 24216/a.ryzhneva/sem2-lab1/lab1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdlib.h>
+
+void print_text(const char* text) {
+    for(int i = 0; i < 10; i++) {
+        fprintf(stderr, "%s\n", text);
+    }
+}
+
+void* thread_body(void* param) {
+    print_text("Child");
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t thread;
+    int code;
+
+    code = pthread_create(&thread, NULL, thread_body, NULL);
+    if (code != 0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof buf);
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+
+    print_text("Parent");
+
+    pthread_exit(NULL);
+}
