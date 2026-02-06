dd5171939f162e4b2fcde7159b03671d7459e070
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2026-02-06 10:23:58 +0700

    fix printf

--- 24216/e.dashkovskii1/sem2-lab2/main.c
+++ 24216/e.dashkovskii1/sem2-lab2/main.c
@@ -2,17 +2,12 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <unistd.h> 
-#include <string.h> 
 
 #define handle_error_en(en, msg) do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
 
 void print_all_lines(const char* string) {
-    char buffer[256];
-    int len = snprintf(buffer, sizeof(buffer), "%s\n", string);
-    
     for (int i = 0; i < 10; i++) {
-        write(STDERR_FILENO, buffer, len); // Пишем в stderr
+        fprintf(stderr, "%s\n", string);
     }
 }
 
@@ -35,7 +30,7 @@ int main() {
     if (result != 0) {
         handle_error_en(result, "pthread_join");
     }
-    
+
     print_all_lines("parent");
 
     exit(EXIT_SUCCESS);
