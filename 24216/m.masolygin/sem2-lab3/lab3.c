6f90effcc8d94ce701ef5b98b56265b6f2ea43e4
Maksim Masolygin <m.masolygin@g.nsu.ru>
2026-02-09 14:07:51 +0700

    feat: add solution

---
+++ 24216/m.masolygin/sem2-lab3/lab3.c
@@ -0,0 +1,76 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+struct package_strings {
+    int cnt;
+    char** strings;
+} typedef package_strings_t;
+
+void print_lines(package_strings_t* package) {
+    for (int i = 0; i < package->cnt; i++) {
+        write(STDOUT_FILENO, package->strings[i], strlen(package->strings[i]));
+        write(STDOUT_FILENO, "\n", 1);
+    }
+}
+
+void* printer_ten(void* arg) {
+    print_lines((package_strings_t*)arg);
+    pthread_exit(NULL);
+}
+
+void create_thread(pthread_t* thread, void* (*start_routine)(void*),
+                   void* arg) {
+    int error = pthread_create(thread, NULL, start_routine, arg);
+
+    if (error != 0) {
+        fprintf(stderr, "Error creating threads: %s\n", strerror(error));
+        exit(1);
+    }
+}
+
+int main() {
+    pthread_t threads[4];
+
+    package_strings_t package0 = {
+        .cnt = 3,
+        .strings = (char*[]){"Thread 0: Line 1", "Thread 0: Line 2",
+                             "Thread 0: Line 3"},
+    };
+
+    package_strings_t package1 = {
+        .cnt = 3,
+        .strings = (char*[]){"Thread 1: Line 1", "Thread 1: Line 2",
+                             "Thread 1: Line 3"},
+    };
+
+    package_strings_t package2 = {
+        .cnt = 3,
+        .strings = (char*[]){"Thread 2: Line 1", "Thread 2: Line 2",
+                             "Thread 2: Line 3"},
+    };
+
+    package_strings_t package3 = {
+        .cnt = 3,
+        .strings = (char*[]){"Thread 3: Line 1", "Thread 3: Line 2",
+                             "Thread 3: Line 3"},
+    };
+
+    create_thread(&threads[0], printer_ten, &package0);
+    create_thread(&threads[1], printer_ten, &package1);
+    create_thread(&threads[2], printer_ten, &package2);
+    create_thread(&threads[3], printer_ten, &package3);
+
+    for (int i = 0; i < 4; i++) {
+        int error = pthread_join(threads[i], NULL);
+        if (error != 0) {
+            fprintf(stderr, "Error joining thread %d: %s\n", i,
+                    strerror(error));
+            exit(1);
+        }
+    }
+
+    return 0;
+}
