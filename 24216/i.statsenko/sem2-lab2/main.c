c9d2b1942642cbc885e38a51bbdc4d1331d3ef12
Ilya Statsenko <i.statsenko@g.nsu.ru>
2026-02-10 23:14:03 +0700

    added solution

--- 24216/i.statsenko/sem2-lab2/main.c
+++ 24216/i.statsenko/sem2-lab2/main.c
@@ -3,20 +3,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-void *printStrings(void *unused) {
-    (void)unused;
+void printStrings() {
     for (int i = 0; i < 10; i++) {
         fprintf(stderr, "Thread: Iteration %d\n", i);
     }
+}
+
+void *thread_print(void *unused) {
+    (void)unused;
+    printStrings();
     pthread_exit(NULL);
 }
 
 int main() {
     pthread_t thread;
-    int err = pthread_create(&thread, NULL, printStrings, NULL);
+    int err = pthread_create(&thread, NULL, thread_print, NULL);
     if (err != 0) {
         fprintf(stderr, "error create thread: %s\n", strerror(err));
         exit(EXIT_FAILURE);
     }
-    printStrings(NULL);
+    err = pthread_join(thread, NULL);
+    if (err != 0) {
+        fprintf(stderr, "error join thread: %s\n", strerror(err));
+        exit(EXIT_FAILURE);
+    }
+    printStrings();
 }
