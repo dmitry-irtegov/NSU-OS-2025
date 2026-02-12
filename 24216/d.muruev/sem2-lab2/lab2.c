080acb678c51017de5f723afe5e1263b0b1b2b49
Denis Muruev <d.muruev@g.nsu.ru>
2026-02-12 20:02:09 +0700

    #

--- 24216/d.muruev/sem2-lab2/lab2.c
+++ 24216/d.muruev/sem2-lab2/lab2.c
@@ -5,29 +5,21 @@
 
 void* print_10_lines(void *arg) {
     for (int i = 0; i < 10; i++) {
-        printf("new thread\n");
+        fprintf(stderr, "new thread\n");
     }
     return NULL;
 }
 
 int main() {
     pthread_t thread;
-    int result;
-    result = pthread_create(&thread, NULL, print_10_lines, NULL);
+    int result = pthread_create(&thread, NULL, print_10_lines, NULL);
     if (result != 0) {
         fprintf(stderr, "Error creating thread: %s\n", strerror(result));
         exit(EXIT_FAILURE);
     }
-
-    result = pthread_join(thread, NULL);
-    if (result != 0) {
-        fprintf(stderr, "Error join: %s\n", strerror(result));
-        exit(EXIT_FAILURE);
-    }
-
+    pthread_join(thread, NULL);
     for (int i = 0; i < 10; i++) {
-        printf("main thread\n");
+        fprintf(stderr, "main thread\n");
     }
-
     return 0;
 }
