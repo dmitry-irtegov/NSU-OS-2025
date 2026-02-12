9e930c429969b7c3de558c209dd195d1f6bfa8f7
Ilya Statsenko <i.statsenko@g.nsu.ru>
2026-02-12 20:06:49 +0700

    fix

--- 24216/i.statsenko/sem2-lab2/main.c
+++ 24216/i.statsenko/sem2-lab2/main.c
@@ -3,21 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-void printStrings() {
+void printStrings(char *str) {
     for (int i = 0; i < 10; i++) {
-        fprintf(stderr, "Thread: Iteration %d\n", i);
+        fprintf(stderr, "Thread %s: Iteration %d\n", str, i);
     }
 }
 
-void *thread_print(void *unused) {
-    (void)unused;
-    printStrings();
+void *thread_print(void *data) {
+    printStrings((char *)data);
     pthread_exit(NULL);
 }
 
 int main() {
     pthread_t thread;
-    int err = pthread_create(&thread, NULL, thread_print, NULL);
+    int err = pthread_create(&thread, NULL, thread_print, "child");
     if (err != 0) {
         fprintf(stderr, "error create thread: %s\n", strerror(err));
         exit(EXIT_FAILURE);
@@ -27,6 +26,6 @@ int main() {
         fprintf(stderr, "error join thread: %s\n", strerror(err));
         exit(EXIT_FAILURE);
     }
-    printStrings();
-    return 0;
+    printStrings("parent");
+    exit(EXIT_SUCCESS);
 }
