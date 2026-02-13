e04c81790aae62afa613c5e231540bf6f628629a
Гаев Владислав <90602583+WorstNormal@users.noreply.github.com>
2026-02-13 08:59:10 +0700

    fix

--- 24216/v.gaev/sem2_lab4/lab4.c
+++ 24216/v.gaev/sem2_lab4/lab4.c
@@ -3,50 +3,60 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 
 void* child_task(void* arg) {
     pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
     pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL);
     while (1) {
-        printf("Дочерняя нить: работает\n");
+        fprintf(stderr, "Дочерняя нить: работает\n");
         sleep(1); 
     }
     return NULL;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    const char *name_prog = argv[0];
+    
     pthread_t thread_id;
     void* result;
     int s;
 
-    printf("Родительская нить: Создание нити\n");
+    fprintf(stderr, "Родительская нить: Создание нити\n");
     s = pthread_create(&thread_id, NULL, child_task, NULL);
     if (s != 0) {
-        errno = s; 
-        perror("pthread_create error");
-        return 1;
+        const char *action = "creating thread";
+        char buf[256];
+        strerror_r(s, buf, sizeof buf);
+        fprintf(stderr, "%s: %s: %s\n", name_prog, action, buf);
+        exit(1);
     }
+    
     sleep(2);
 
-    printf("Родительская нить: Отправка сигнала отмены\n");
+    fprintf(stderr, "Родительская нить: Отправка сигнала отмены\n");
     s = pthread_cancel(thread_id);
     if (s != 0) {
-        errno = s;
-        perror("pthread_cancel error");
-        return 1;
+        const char *action = "canceling thread";
+        char buf[256];
+        strerror_r(s, buf, sizeof buf);
+        fprintf(stderr, "%s: %s: %s\n", name_prog, action, buf);
+        exit(1);
     }
 
     s = pthread_join(thread_id, &result);
     if (s != 0) {
-        errno = s;
-        perror("pthread_join error");
-        return 1;
+        const char *action = "joining thread";
+        char buf[256];
+        strerror_r(s, buf, sizeof buf);
+        fprintf(stderr, "%s: %s: %s\n", name_prog, action, buf);
+        exit(1);
     }
 
     if (result == PTHREAD_CANCELED) {
-        printf("Родительская нить: Нить была корректно отменена.\n");
+        fprintf(stderr, "Родительская нить: Нить была корректно отменена.\n");
     } else {
-        printf("Родительская нить: Нить завершилась сама. \n");
+        fprintf(stderr, "Родительская нить: Нить завершилась сама.\n");
     }
 
     return 0;
