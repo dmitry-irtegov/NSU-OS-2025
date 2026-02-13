6af6a0a9ee54e4f58ad77a8e019eb2868fde99d8
Масолыгин Максим Олегович <117579740+KrutoiArbuz@users.noreply.github.com>
2026-02-13 09:27:52 +0700

    sem2-lab1 m.masolygin24216 (#572)
    * lab1
    
    * fix: remove excess
    
    * fix: stderr/stdout thread

---
+++ 24216/m.masolygin/sem2-lab1/lab1.c
@@ -0,0 +1,29 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_lines(char* str) {
+    for (int i = 1; i <= 10; i++) {
+        fprintf(stderr, "%s %d\n", str, i);
+    }
+}
+
+void* printer_ten(void* arg) {
+    print_lines((char*)arg);
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t thread;
+    int error = pthread_create(&thread, NULL, printer_ten, "Child string:");
+
+    if (error != 0) {
+        fprintf(stderr, "Error creating threads: %s\n", strerror(error));
+        return 1;
+    }
+
+    print_lines("Parent string:");
+
+    pthread_exit(NULL);
+}
