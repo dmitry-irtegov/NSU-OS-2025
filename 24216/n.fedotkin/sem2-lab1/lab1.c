ea51e5b3519fa5adadbd3250edbd28f5d950a6f7
Samyj krutoj arbuz <166312933+Avtobot22@users.noreply.github.com>
2026-02-13 09:39:07 +0700

    sem2 lab1 (#574)
    * lab1 sem2
    
    * lab1 sem2 fix
    
    * fix

---
+++ 24216/n.fedotkin/sem2-lab1/lab1.c
@@ -0,0 +1,34 @@
+#include "pthread.h"
+#include "stdio.h"
+#include <stdlib.h>
+#include <string.h>
+
+void* thread_printer(void *arg) {
+    for (int i = 0; i < 10; i++) {
+        fprintf(stderr, "Child thread: %d\n", i);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    int result = 0;
+
+    result = pthread_create(&thread, NULL, thread_printer, NULL);
+    if (result != 0) {
+        fprintf(stderr, "Error pthread_create: %s\n", strerror(result));
+        return 1;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        fprintf(stderr, "Main thread: %d\n", i);
+    }
+
+    result = pthread_join(thread, NULL);
+    if (result != 0) {
+        fprintf(stderr, "Error pthread_join: %s\n", strerror(result));
+        return 1;
+    }
+
+    return 0;
+}
