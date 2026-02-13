d0046100d3fbafa37a03bf31c8bcd524b29fa3c8
Egor <117298302+DevDashkovsky@users.noreply.github.com>
2026-02-13 10:38:39 +0700

    Sem2 lab2 Dashkovsky (#576)
    * sem2-lab2
    
    * sem2-lab2
    
    * fix printf
    
    * fix printf
    
    * fix printf
    
    * del makefile

---
+++ 24216/e.dashkovskii1/sem2-lab2/main.c
@@ -0,0 +1,37 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#define handle_error_en(en, msg) do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+
+void print_all_lines(const char* string) {
+    for (int i = 0; i < 10; i++) {
+        fprintf(stderr, "%s\n", string);
+    }
+}
+
+void *print_lines(void *arg) {
+    (void)arg;
+    print_all_lines("thread");
+    return NULL;
+}
+
+int main() {
+    pthread_t tid;
+    int result;
+
+    result = pthread_create(&tid, NULL, &print_lines, NULL);
+    if (result != 0) {
+        handle_error_en(result, "pthread_create");
+    }
+
+    result = pthread_join(tid, NULL);
+    if (result != 0) {
+        handle_error_en(result, "pthread_join");
+    }
+
+    print_all_lines("parent");
+
+    exit(EXIT_SUCCESS);
+}
