bd8976713c70ac2e491de5a8776620f8f5b0c9cf
Илья Стаценко <97968823+tipo-tawerka@users.noreply.github.com>
2026-02-13 09:21:28 +0700

    Sem2-lab1 (#570)
    * fix
    
    * fix
    
    * fix
    
    * fix

---
+++ 24216/i.statsenko/sem2-lab1/main.c
@@ -0,0 +1,22 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void *printStrings(void *unused) {
+    (void)unused;
+    for (int i = 0; i < 10; i++) {
+        fprintf(stderr, "Thread: Iteration %d\n", i);
+    }
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t thread;
+    int err = pthread_create(&thread, NULL, printStrings, NULL);
+    if (err != 0) {
+        fprintf(stderr, "error create thread: %s\n", strerror(err));
+        exit(EXIT_FAILURE);
+    }
+    printStrings(NULL);
+}
