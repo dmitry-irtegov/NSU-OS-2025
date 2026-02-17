e5d74341e19526a3578bf22c83a3db518173feee
Rakdat <makstokarev930@gmail.com>
2026-02-17 10:05:49 +0700

    lab1

---
+++ 24215/m.tokarev4/sem2-lab1/lab1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* print10lines(void* arg) {
+    char* text = (char*)arg;
+    for (int i = 1; i <= 10; i++) {
+        printf("%d - %s\n", i, text);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    int status;
+
+    status = pthread_create(&thread, NULL, print10lines, "D");
+    if (status != 0) {
+        fprintf(stderr, "Error creating thread\n");
+        return 1;
+   }
+
+    print10lines("P");
+
+    //pthread_join(thread, NULL);
+    pthread_exit(NULL);
+    return 0;
+}
+
