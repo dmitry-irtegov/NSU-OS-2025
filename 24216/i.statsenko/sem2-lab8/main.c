2ad4b50e5c74cc5066ea7328fb1b9afad7bee3d2
Ilya Statsenko <i.statsenko@g.nsu.ru>
2026-02-19 17:47:57 +0700

    fix

--- 24216/i.statsenko/sem2-lab8/main.c
+++ 24216/i.statsenko/sem2-lab8/main.c
@@ -19,7 +19,7 @@ void *threadComputer(void *data) {
     double *partialSum = malloc(sizeof(double));
     if (partialSum == NULL) {
         perror("malloc in threadComputer");
-        exit(EXIT_FAILURE);
+        pthread_exit(NULL);
     }
     *partialSum = 0.0;
 
@@ -27,19 +27,19 @@ void *threadComputer(void *data) {
         *partialSum += 1.0 / (i * 4.0 + 1.0);
         *partialSum -= 1.0 / (i * 4.0 + 3.0);
     }
-    free(threadData);
     pthread_exit(partialSum);
 }
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        printf("Usage: %s <number of threads>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <number of threads>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    int countThread = atoi(argv[1]);
-    if (countThread <= 0) {
-        printf("Number of threads must be a positive integer.\n");
+    char *endptr;
+    long countThread = strtol(argv[1], &endptr, 10);
+    if (*endptr != '\0' || countThread <= 0) {
+        fprintf(stderr, "Number of threads must be a positive integer.\n");
         exit(EXIT_FAILURE);
     }
 
@@ -48,32 +48,37 @@ int main(int argc, char** argv) {
         perror("malloc");
         exit(EXIT_FAILURE);
     }
+    ThreadData *dataArray = malloc(countThread * sizeof(ThreadData));
+    if (dataArray == NULL) {
+        perror("malloc");
+        free(threads);
+        exit(EXIT_FAILURE);
+    }
 
-    for (int i = 0; i < countThread; i++) {
-        ThreadData *data = malloc(sizeof(ThreadData));
-        if (data == NULL) {
-            perror("malloc");
-            exit(EXIT_FAILURE);
-        }
-        data->start = i * (num_steps / countThread);
+    for (long i = 0; i < countThread; i++) {
+        dataArray[i].start = i * (num_steps / countThread);
         if (i == countThread - 1) {
-            data->end = num_steps;
+            dataArray[i].end = num_steps;
         } else {
-            data->end = (i + 1) * (num_steps / countThread);
+            dataArray[i].end = (i + 1) * (num_steps / countThread);
         }
-        int err = pthread_create(&threads[i], NULL, threadComputer, data);
+        int err = pthread_create(&threads[i], NULL, threadComputer, &dataArray[i]);
         if (err != 0) {
-            fprintf(stderr, "Error creating thread %d: %s\n", i, strerror(err));
+            fprintf(stderr, "Error creating thread %ld: %s\n", i, strerror(err));
             exit(EXIT_FAILURE);
         }
     }
 
     double pi = 0.0;
-    for (int i = 0; i < countThread; i++) {
+    for (long i = 0; i < countThread; i++) {
         void *partialSum;
         int err = pthread_join(threads[i], &partialSum);
         if (err != 0) {
-            fprintf(stderr, "Error joining thread %d: %s\n", i, strerror(err));
+            fprintf(stderr, "Error joining thread %ld: %s\n", i, strerror(err));
+            exit(EXIT_FAILURE);
+        }
+        if (partialSum == NULL) {
+            fprintf(stderr, "Thread %ld returned NULL partial sum\n", i);
             exit(EXIT_FAILURE);
         }
         pi += *(double *)partialSum;
@@ -82,5 +87,6 @@ int main(int argc, char** argv) {
     pi *= 4.0;
     fprintf(stderr, "Pi = %.15g\n", pi);
     free(threads);
+    free(dataArray);
     return 0;
 }
