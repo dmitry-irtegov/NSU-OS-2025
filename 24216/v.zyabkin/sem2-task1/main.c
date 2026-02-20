8c7eb6840110e4bbc55d37d867009e886b94bcea
Ziabkin Vladimir <150025985+maket12@users.noreply.github.com>
2026-02-20 10:37:33 +0700

    Sem2-Task1 (#590)
    * add solution via putenv (#36)
    
    * uploaded solution
    
    * deleted useless trash
    
    * some changes
    
    * Improve error message in thread creation
    
    Updated error handling to use strerror for better clarity.
    
    ---------
    
    Co-authored-by: Matvey Solovyov <83802608+fresh-ops@users.noreply.github.com>

---
+++ 24216/v.zyabkin/sem2-task1/main.c
@@ -0,0 +1,32 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void thread_task(char *str) {
+    for (int i = 1; i <= 10; i++) {
+        fprintf(stderr, "%s%d\n", str, i);
+    }
+}
+
+void* launch_thread(void* arg) {
+    thread_task((char*)arg);
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t thread_id;
+    int result;
+
+    result = pthread_create(&thread_id, NULL, launch_thread, "Child thread: string №");
+
+    if (result != 0) {
+        fprintf(stderr, "Error while creating a thread: %s\n", strerror(result));
+        exit(1);
+    }
+
+    thread_task("Parent thread: string №");
+
+    pthread_exit(NULL);
+
+}
