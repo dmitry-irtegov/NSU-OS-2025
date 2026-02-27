ba94e63819fcf5605967b2a67c732d5f60619e50
rend <stepan.efimov2006@mail.ru>
2026-02-27 10:46:37 +0700

    task3 (#626)
    * task3
    
    * fix printf
    
    * change name dir

---
+++ 24216/s.efimov/sem2-lab3/lab3.c
@@ -0,0 +1,55 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char **sentences;
+    int count;
+    int thread_num;
+} thread_data_t;
+
+void* print_strings(void* arg) {
+    thread_data_t *data = (thread_data_t*)arg;
+    
+    for (int i = 0; i < data->count; i++) {
+        fprintf(stderr, "Поток %d: %s\n", data->thread_num, data->sentences[i]);
+    }
+    
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t threads[4];
+    thread_data_t t_data[4];
+
+    char *list0[] = {"Яблоко", "Груша"};
+    char *list1[] = {"Кот", "Собака", "Рыба"};
+    char *list2[] = {"Чёрный", "Красный", "Белый", "Синий"};
+    char *list3[] = {"Понедельник", "Вторник"};
+
+    char **all_lists[] = {list0, list1, list2, list3};
+    int counts[] = {2, 3, 4, 2};
+
+    for (int i = 0; i < 4; i++) {
+        t_data[i].sentences = all_lists[i];
+        t_data[i].count = counts[i];
+        t_data[i].thread_num = i + 1;
+
+        int status = pthread_create(&threads[i], NULL, print_strings, &t_data[i]);
+        if (status != 0) {
+            fprintf(stderr, "Ошибка создания потока %d: %s\n", i, strerror(status));
+            break; 
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        int status = pthread_join(threads[i], NULL);
+        if (status != 0) {
+            fprintf(stderr, "Ошибка join для потока %d: %s\n", i, strerror(status));
+        }
+    }
+
+    fprintf(stderr, "Все потоки завершили работу.\n");
+    return 0;
+}
