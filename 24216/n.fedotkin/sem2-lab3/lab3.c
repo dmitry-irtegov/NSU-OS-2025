1f6c5b9d8b53c3a8acca42b7ba5ed4ae9ef3c881
Samyj krutoj arbuz <166312933+Avtobot22@users.noreply.github.com>
2026-02-27 10:14:21 +0700

    Sem2 lab3 (#701)
    * sem2 lab3
    
    * fix

---
+++ 24216/n.fedotkin/sem2-lab3/lab3.c
@@ -0,0 +1,45 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+const char* string1[] = {"First: I\n", "First: love\n", "First: OSI\n", NULL};
+const char* string2[] = {"Second: I\n", "Second: love\n", "Second: OSI\n", NULL};
+const char* string3[] = {"Third: I\n", "Third: love\n", "Third: OSI\n", NULL};
+const char* string4[] = {"Fourth: I\n", "Fourth: love\n", "Fourth: OSI\n", NULL};
+
+const char** strings[] = {string1, string2, string3, string4};
+
+void* thread_printer(void* arg) {
+    const char** lines = (const char** )arg;
+
+    for (int i = 0; lines[i] != NULL; i++) {
+        fprintf(stderr, "%s", lines[i]);
+    }
+
+    return NULL;
+}
+
+int main() {
+    pthread_t workers[4];
+
+    for (int i = 0; i < 4; i++) {
+        int status = pthread_create(&workers[i], NULL, thread_printer, (void* )strings[i]);
+        
+        if (status != 0) {
+            fprintf(stderr, "Error: pthread_create: %s", strerror(status));
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        int res = pthread_join(workers[i], NULL);
+        if (res != 0) {
+            fprintf(stderr, "Error: pthread_join: %s", strerror(res));
+            return 1;
+        }
+    }
+
+    return 0;
+}
