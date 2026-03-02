6287b368593bea71cbf76b02478e9ab6c0758805
Tokarev Maksim <m.tokarev3@g.nsu.ru>
2026-03-02 13:01:19 +0700

    Init commmit for sem2-lab8

---
+++ 24214/m.tokarev3/sem2-lab8/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const totalIterations = 1_000_000_000
+
+func calcPartialPi(start, end int, ch chan<- float64) {
+	var sum float64 = 0
+	for k := start; k < end; k++ {
+		term := 1.0 / float64(2*k+1)
+
+		if k%2 != 0 {
+			sum -= term
+		} else {
+			sum += term
+		}
+	}
+
+	ch <- sum
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go {num of goroutines}")
+		return
+	}
+
+	numOfPthreads, err := strconv.Atoi(os.Args[1])
+	if err != nil || numOfPthreads <= 0 {
+		fmt.Println("Num of goroutines error: incorrect value")
+		return
+	}
+
+	ch := make(chan float64, numOfPthreads)
+
+	chunkSize := totalIterations / numOfPthreads
+
+	for i := 0; i < numOfPthreads; i++ {
+		start := i * chunkSize
+		end := start + chunkSize
+
+		if i == numOfPthreads-1 {
+			end = totalIterations
+		}
+
+		go calcPartialPi(start, end, ch)
+	}
+
+	var totalSum float64 = 0
+
+	for i := 0; i < numOfPthreads; i++ {
+		totalSum += <-ch
+	}
+
+	pi := totalSum * 4.0
+
+	fmt.Printf("Pi = %.15f\n", pi)
+}
