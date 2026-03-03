db194ae4651e94f7fff0abd6d86f88af197944e5
zelizavetta <e.zaitseva2@g.nsu.ru>
2026-03-03 13:39:58 +0700

    sem2-lab2

---
+++ sem2-lab2/lab2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+
+void* thread_function(void* arg) {
+    for (int i = 1; i <= 10; i++) {
+        fprintf(stderr, "Child line %d\n", i);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+
+    int result = pthread_create(&thread, NULL, thread_function, NULL);
+    if (result != 0) {
+        char buf[256];
+        strerror_r(result, buf, sizeof(buf));
+        fprintf(stderr, "creating thread: %s\n", buf);
+        return EXIT_FAILURE;
+    }
+
+    int join_result = pthread_join(thread, NULL);
+    if (join_result != 0) {
+        char buf[256];
+        strerror_r(join_result, buf, sizeof(buf));
+        fprintf(stderr, "Error joining: %s\n", buf);
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 1; i <= 10; i++) {
+        fprintf(stderr, "Parent line %d\n", i);
+    }
+
+    return EXIT_SUCCESS;
+}
