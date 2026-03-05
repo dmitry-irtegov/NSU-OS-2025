282636f8f5fabd5aca33d6a46d341dbd3ac7125b
Vladislav Munkuev <v.munkuev@g.nsu.ru>
2026-03-05 20:30:06 +0700

    solution

---
+++ 24215/v.munkuev/sem2-lab01/sem2-lab01.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+void* func(void* arg){
+    int *arg_value = (int *)arg;
+    for (int i = 0; i < *arg_value; i++){
+        printf("%d text text\n", i);
+    }
+
+    return NULL;
+}
+
+int main(){
+    int arg = 10;
+    pthread_t TID;
+
+    int res = pthread_create(&TID, NULL, func, &arg);
+    if (res){
+        fprintf(stderr, "pthread error\n");
+        return 1;
+    }
+    
+    //pthread_join(TID, NULL);
+    printf("parent:\n");
+    func(&arg); 
+    
+    pthread_exit(NULL);
+
+    return 0;
+}
