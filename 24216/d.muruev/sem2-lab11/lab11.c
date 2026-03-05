f8fbfb5846a49fe41df29ca5f95a2a347f8c39b4
Denis Muruev <d.muruev@g.nsu.ru>
2026-03-05 23:57:25 +0700

    fix

--- 24216/d.muruev/sem2-lab11/lab11.c
+++ 24216/d.muruev/sem2-lab11/lab11.c
@@ -1,103 +1,74 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <string.h>
 #include <unistd.h>
 
-pthread_mutex_t m1;
-pthread_mutex_t m2;
+pthread_mutex_t parent_mutex;
+pthread_mutex_t child_mutex;
+pthread_mutex_t sync_mutex;
+
+void release_to_child() {
+    pthread_mutex_unlock(&child_mutex);
+    pthread_mutex_lock(&sync_mutex);
+    pthread_mutex_unlock(&parent_mutex);
+}
+
+void release_to_parent() {
+    pthread_mutex_lock(&child_mutex);
+    pthread_mutex_unlock(&sync_mutex);
+    pthread_mutex_lock(&parent_mutex);
+}
 
 void* print_10_lines(void *arg) {
     (void)arg;
-    pthread_mutex_t *my = &m2;
-    pthread_mutex_t *other = &m1;
-    pthread_mutex_t *tmp;
-
-    if (pthread_mutex_lock(my) != 0) {
-        perror("child lock init");
-        return NULL;
-    }
-
+    pthread_mutex_lock(&sync_mutex);
     for (int i = 0; i < 10; i++) {
-        if (pthread_mutex_lock(other) != 0) {
-            perror("child lock other");
-            return NULL;
-        }
-
+        release_to_parent();
         fprintf(stderr, "new thread\n");
-        
-        if (pthread_mutex_unlock(my) != 0) {
-            perror("child unlock my");
-            return NULL;
-        }
-        
-        usleep(10);
-        
-        tmp = my;
-        my = other;
-        other = tmp;
+        release_to_child();
     }
-    
-    pthread_mutex_unlock(my);
-
+    pthread_mutex_unlock(&sync_mutex);
     return NULL;
 }
 
 int main() {
-    pthread_mutexattr_t m_attr;
-    pthread_mutexattr_init(&m_attr);
-    pthread_mutexattr_settype(&m_attr, PTHREAD_MUTEX_ERRORCHECK);
-    
-    if (pthread_mutex_init(&m1, &m_attr) != 0 || pthread_mutex_init(&m2, &m_attr) != 0) {
-        perror("mutex init");
-        exit(EXIT_FAILURE);
-    }
-    pthread_mutexattr_destroy(&m_attr);
-
-    if (pthread_mutex_lock(&m1) != 0) {
-        perror("parent lock init");
-        exit(EXIT_FAILURE);
-    }
-
     pthread_t thread;
+
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
+
+    pthread_mutex_init(&parent_mutex, &attr);
+    pthread_mutex_init(&child_mutex, &attr);
+    pthread_mutex_init(&sync_mutex, &attr);
+
+    pthread_mutex_lock(&parent_mutex);
+    pthread_mutex_lock(&child_mutex);
+
     int result = pthread_create(&thread, NULL, print_10_lines, NULL);
     if (result != 0) {
         fprintf(stderr, "Error creating thread: %s\n", strerror(result));
         exit(EXIT_FAILURE);
     }
 
-    usleep(10000); 
-
-    pthread_mutex_t *my = &m1;
-    pthread_mutex_t *other = &m2;
-    pthread_mutex_t *tmp;
+    while (pthread_mutex_trylock(&sync_mutex) == 0) {
+        pthread_mutex_unlock(&sync_mutex);  
+    }
 
     for (int i = 0; i < 10; i++) {
         fprintf(stderr, "main thread\n");
-        
-        if (pthread_mutex_unlock(my) != 0) {
-             perror("parent unlock my");
-             exit(EXIT_FAILURE);
-        }
-        
-        usleep(10);
-
-        if (pthread_mutex_lock(other) != 0) {
-             perror("parent lock other");
-             exit(EXIT_FAILURE);
-        }
-
-        tmp = my;
-        my = other;
-        other = tmp;
+        release_to_child();
+        release_to_parent();
     }
 
-    pthread_mutex_unlock(my);
+    pthread_join(thread, NULL);
 
-    pthread_join(thread, NULL); 
+    pthread_mutex_destroy(&parent_mutex);
+    pthread_mutex_destroy(&child_mutex);
+    pthread_mutex_destroy(&sync_mutex);
     
-    pthread_mutex_destroy(&m1);
-    pthread_mutex_destroy(&m2);
+    pthread_mutexattr_destroy(&attr);
 
-    pthread_exit(NULL);
+    return EXIT_SUCCESS;
 }
