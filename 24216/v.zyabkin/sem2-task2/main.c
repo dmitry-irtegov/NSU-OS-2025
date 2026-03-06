614f6d4cf721bf3212b71565741b04538e8732b2
Ziabkin Vladimir <150025985+maket12@users.noreply.github.com>
2026-03-06 10:27:47 +0700

    Sem2 task2 (#671)
    * add solution via putenv (#36)
    
    * uploaded solution
    
    * fixes
    
    * deleted some trash
    
    * deleted trash
    
    ---------
    
    Co-authored-by: Matvey Solovyov <83802608+fresh-ops@users.noreply.github.com>

---
+++ 24216/v.zyabkin/sem2-task2/main.c
@@ -0,0 +1,37 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void thread_task(char *str) {
+    for (int i = 1; i <= 10; i++) {
+        fprintf(stderr, "%s%d\n", str, i);
+    }
+}
+
+void* launch_thread(void* arg) {
+    thread_task((char*)arg);
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t thread_id;
+    int result;
+
+    result = pthread_create(&thread_id, NULL, launch_thread, "Child thread: string №");
+
+    if (result != 0) {
+        fprintf(stderr, "Error while creating a thread: %s\n", strerror(result));
+        exit(1);
+    }
+
+    result = pthread_join(thread_id, NULL);
+    if (result != 0) {
+        fprintf(stderr, "Error while joining a thread: %s\n", strerror(result));
+        exit(1);
+    }
+q
+    thread_task("Parent thread: string №");
+
+    return 0;
+}
