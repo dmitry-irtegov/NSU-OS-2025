8fd31151455e5a2b0dae00f94c3d37f63c3b8528
Yuriy Pivkin <58018381+alwenwhy@users.noreply.github.com>
2026-03-10 15:00:56 +0700

    24215 Pivkin sem2-lab1 (#592)
    * sem2-lab1 done
    
    * Update lab1.c

---
+++ 24215/y.pivkin/sem2-lab1/lab1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* thread_func() {
+    for (int i = 0; i < 10; i++) {
+        printf("Child thread %d\n", i + 1);
+    }
+
+    return NULL;
+}
+
+int main () {
+    pthread_t thread;
+
+    if (pthread_create(&thread, NULL, thread_func, NULL) != 0) {
+        perror("pthread_create");
+        exit(1);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("Parent thread %d\n", i + 1);
+    }
+
+    pthread_exit(NULL);
+    //exit(0);
+}
