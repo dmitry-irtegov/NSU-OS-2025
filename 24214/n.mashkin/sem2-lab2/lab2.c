196b3a2f8ab56e271055a5184374ce468d9f3f0b
Nikolay Mashkin <n.mashkin@g.nsu.ru>
2026-03-16 11:28:38 +0700

    lab2 sem2

---
+++ 24214/n.mashkin/sem2-lab2/lab2.c
@@ -0,0 +1,30 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+
+void *thread(void *) {
+    for (int i = 0; i < 10; i++) {
+        printf("Child: %d\n", i);
+    }
+}
+
+int main() {
+    pthread_t thr;
+
+    int res;
+    if ((res = pthread_create(&thr, NULL, thread, NULL)) != 0) {
+        fprintf(stderr, "pthread_create: %s", strerror(res));
+        return 1;
+    }
+
+    if ((res = pthread_join(thr, NULL) != 0)) {
+        fprintf(stderr, "pthread_join: %s", strerror(res));
+        return 1;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("Parent: %d\n", i);
+    }
+
+    return 0;
+}
