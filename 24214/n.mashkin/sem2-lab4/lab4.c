18d51649bc0e713e4d4f99abb1301d810f00cc8e
Nikolay Mashkin <n.mashkin@g.nsu.ru>
2026-03-16 11:30:21 +0700

    lab4 sem2

---
+++ 24214/n.mashkin/sem2-lab4/lab4.c
@@ -0,0 +1,31 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void *thread(void *) {
+    for (int i = 0; i < 100; i++) {
+        printf("Child: %d\n", i);
+        usleep(300000);
+    }
+}
+
+int main() {
+    pthread_t thr;
+
+    int res;
+    if ((res = pthread_create(&thr, NULL, thread, NULL)) != 0) {
+        fprintf(stderr, "pthread_create: %s", strerror(res));
+        return 1;
+    }
+
+    sleep(2);
+
+    if ((res = pthread_cancel(thr)) != 0) {
+        fprintf(stderr, "pthread_cancel: %s", strerror(res));
+        return 1;
+    }
+
+    return 0;
+}
+
