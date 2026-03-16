42e3b40b7d8d388672d53e6ded9c911335a9d626
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2026-03-16 15:52:17 +0700

    fix

--- 24216/e.dashkovskii1/sem2-lab11/main.c
+++ 24216/e.dashkovskii1/sem2-lab11/main.c
@@ -6,9 +6,7 @@
 #include <stdlib.h>
 #include <sched.h>
 
-pthread_mutex_t m_sync;
-pthread_mutex_t m_parent;
-pthread_mutex_t m_child;
+pthread_mutex_t m[3];
 
 volatile int child_ready = 0;
 
@@ -23,23 +21,18 @@ void handle_error(int en, const char* msg) {
 void *print_lines(void *arg) {
     (void)arg;
 
-    handle_error(pthread_mutex_lock(&m_child), "child: initial lock m_child");
+    handle_error(pthread_mutex_lock(&m[2]), "child: initial lock m[2]");
     child_ready = 1;
 
     for (int i = 0; i < 10; i++) {
-        handle_error(pthread_mutex_lock(&m_parent), "child: lock m_parent");
-        handle_error(pthread_mutex_unlock(&m_child), "child: unlock m_child");
-
+        handle_error(pthread_mutex_lock(&m[i % 3]), "child: lock next");
+        
         fprintf(stderr, "thread\n");
 
-        handle_error(pthread_mutex_lock(&m_sync), "child: lock m_sync");
-        handle_error(pthread_mutex_unlock(&m_parent), "child: unlock m_parent");
-
-        handle_error(pthread_mutex_lock(&m_child), "child: lock m_child");
-        handle_error(pthread_mutex_unlock(&m_sync), "child: unlock m_sync");
+        handle_error(pthread_mutex_unlock(&m[(i + 2) % 3]), "child: unlock current");
     }
 
-    handle_error(pthread_mutex_unlock(&m_child), "child: final unlock");
+    handle_error(pthread_mutex_unlock(&m[9 % 3]), "child: final unlock");
     return NULL;
 }
 
@@ -50,13 +43,13 @@ int main() {
     handle_error(pthread_mutexattr_init(&attr), "attr_init");
     handle_error(pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK), "attr_settype");
     
-    handle_error(pthread_mutex_init(&m_sync, &attr), "init m_sync");
-    handle_error(pthread_mutex_init(&m_parent, &attr), "init m_parent");
-    handle_error(pthread_mutex_init(&m_child, &attr), "init m_child");
+    for (int i = 0; i < 3; i++) {
+        handle_error(pthread_mutex_init(&m[i], &attr), "init mutex");
+    }
     
     handle_error(pthread_mutexattr_destroy(&attr), "attr_destroy");
 
-    handle_error(pthread_mutex_lock(&m_parent), "main: initial lock m_parent");
+    handle_error(pthread_mutex_lock(&m[0]), "main: initial lock m[0]");
 
     int result = pthread_create(&tid, NULL, &print_lines, NULL);
     handle_error(result, "pthread_create");
@@ -68,24 +61,18 @@ int main() {
     for (int i = 0; i < 10; i++) {
         fprintf(stderr, "parent\n");
 
-        handle_error(pthread_mutex_lock(&m_sync), "main: lock m_sync");
-        handle_error(pthread_mutex_unlock(&m_parent), "main: unlock m_parent");
-
-        handle_error(pthread_mutex_lock(&m_child), "main: lock m_child");
-        handle_error(pthread_mutex_unlock(&m_sync), "main: unlock m_sync");
-
-        handle_error(pthread_mutex_lock(&m_parent), "main: lock m_parent");
-        handle_error(pthread_mutex_unlock(&m_child), "main: unlock m_child");
+        handle_error(pthread_mutex_lock(&m[(i + 1) % 3]), "main: lock next");
+        handle_error(pthread_mutex_unlock(&m[i % 3]), "main: unlock current");
     }
 
-    handle_error(pthread_mutex_unlock(&m_parent), "main: final unlock");
+    handle_error(pthread_mutex_unlock(&m[10 % 3]), "main: final unlock");
 
     result = pthread_join(tid, NULL);
     handle_error(result, "pthread_join");
 
-    pthread_mutex_destroy(&m_child);
-    pthread_mutex_destroy(&m_parent);
-    pthread_mutex_destroy(&m_sync);
+    for (int i = 0; i < 3; i++) {
+        pthread_mutex_destroy(&m[i]);
+    }
 
     exit(EXIT_SUCCESS);
 }
