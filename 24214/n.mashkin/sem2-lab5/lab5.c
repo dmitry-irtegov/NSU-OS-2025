319b3631ba1a8780ad3d6a369e35ccfe98cea71e
Nikolay Mashkin <n.mashkin@g.nsu.ru>
2026-03-16 11:31:17 +0700

    lab5 sem2

---
+++ 24214/n.mashkin/sem2-lab5/lab5.c
@@ -0,0 +1,44 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void goddamn(void *) {
+    printf("GODDAMN\n");
+}
+
+void *thread(void *) {
+    pthread_cleanup_push(goddamn, NULL);
+
+    for (int i = 0; i < 100; i++) {
+        printf("Child: %d\n", i);
+        usleep(300000);
+    }
+
+    pthread_cleanup_pop(1);
+}
+
+int main() {
+    pthread_t thr;
+
+    int res;
+    if ((res = pthread_create(&thr, NULL, thread, NULL)) != 0) {
+        fprintf(stderr, "pthread_create: %s", strerror(res));
+        return 1;
+    }
+
+    sleep(2);
+
+    if ((res = pthread_cancel(thr)) != 0) {
+        fprintf(stderr, "pthread_cancel: %s", strerror(res));
+        return 1;
+    }
+
+    if ((res = pthread_join(thr, NULL)) != 0) {
+        fprintf(stderr, "pthread_join: %s", strerror(res));
+        return 1;
+    }
+
+    return 0;
+}
+
