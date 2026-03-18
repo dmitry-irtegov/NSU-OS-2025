c5cefc2038b679ce6fdc7276d40dc8bff5df45ec
Kirpich Mira <k.maltsev@g.nsu.ru>
2026-03-18 13:07:08 +0700

    fix code

--- 24215/k.maltsev/sem2-lab9/pi_calculation_modern.c
+++ 24215/k.maltsev/sem2-lab9/pi_calculation_modern.c
@@ -3,16 +3,14 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <pthread.h>
-#include <unistd.h>
 
-#define CHUNK_STEPS 200000000L
 #define CHECK_INTERVAL 1000000L
 
 static volatile sig_atomic_t stop_requested = 0;
 
 typedef struct {
-    long start_step;
-    long max_steps;
+    long thread_id;
+    long num_threads;
 } thread_data_t;
 
 static void sigint_handler(int signo)
@@ -24,108 +22,22 @@ static void sigint_handler(int signo)
 void *calc_pi(void *arg)
 {
     thread_data_t *data = (thread_data_t *)arg;
-    long start = data->start_step;
-    long max_steps = data->max_steps;
+    long id = data->thread_id;
+    long n_threads = data->num_threads;
 
-    double *partial_sum = (double *)malloc(sizeof(double));
+    double *partial_sum = malloc(sizeof(double));
     if (partial_sum == NULL) {
-        perror("malloc error in thread");
         pthread_exit(NULL);
     }
+
     *partial_sum = 0.0;
 
-    long i;
-    for (i = 0; i < max_steps; ++i) {
-        long k = start + i;
-        double term1 = 1.0 / (k * 4.0 + 1.0);
-        double term2 = 1.0 / (k * 4.0 + 3.0);
-        *partial_sum += term1 - term2;
-
-        if ((i % CHECK_INTERVAL) == 0) {
-            if (stop_requested) {
-                long extra = CHECK_INTERVAL;
-                long j;
-                for (j = 0; j < extra && (i + j) < max_steps; ++j) {
-                    long kk = start + i + j;
-                    double t1 = 1.0 / (kk * 4.0 + 1.0);
-                    double t2 = 1.0 / (kk * 4.0 + 3.0);
-                    *partial_sum += t1 - t2;
-                }
-                i += extra;
-                break;
-            }
-        }
-    }
-
-    pthread_exit((void *)partial_sum);
-}
-
-int main(int argc, char **argv)
-{
-    if (argc < 2) {
-        fprintf(stderr, "Использование: %s <количество потоков>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    int num_threads = atoi(argv[1]);
-    if (num_threads <= 0) {
-        fprintf(stderr, "Количество потоков должно быть положительным числом.\n");
-        return EXIT_FAILURE;
-    }
-
-    struct sigaction sa;
-    sa.sa_handler = sigint_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    if (sigaction(SIGINT, &sa, NULL) == -1) {
-        perror("sigaction");
-        return EXIT_FAILURE;
-    }
-
-    pthread_t *threads = (pthread_t *)malloc(num_threads * sizeof(pthread_t));
-    thread_data_t *thread_data = (thread_data_t *)malloc(num_threads * sizeof(thread_data_t));
-    if (threads == NULL || thread_data == NULL) {
-        perror("malloc error");
-        free(threads);
-        free(thread_data);
-        return EXIT_FAILURE;
-    }
-
-    double pi_sum = 0.0;
-    long current_step = 0;
-
-    while (!stop_requested) {
-        for (int i = 0; i < num_threads; ++i) {
-            thread_data[i].start_step = current_step + i * (CHUNK_STEPS / num_threads);
-            thread_data[i].max_steps  = CHUNK_STEPS / num_threads;
-        }
-
-        long base = CHUNK_STEPS / num_threads;
-        long rest = CHUNK_STEPS - base * num_threads;
-        thread_data[num_threads - 1].max_steps += rest;
-
-        current_step += CHUNK_STEPS;
-
-        for (int i = 0; i < num_threads; ++i) {
-            if (pthread_create(&threads[i], NULL, calc_pi, &thread_data[i]) != 0) {
-                perror("pthread_create error");
-                stop_requested = 1;
-                num_threads = i;
-                break;
-            }
-        }
-
-        for (int i = 0; i < num_threads; ++i) {
-            void *status = NULL;
-            if (pthread_join(threads[i], &status) != 0) {
-                perror("pthread_join error");
-                continue;
-            }
-            if (status != NULL) {
-                double *sum_from_thread = (double *)status;
-                pi_sum += *sum_from_thread;
-                free(sum_from_thread);
-            }
+    long i = id;
+    while (1) {
+        long limit = i + CHECK_INTERVAL * n_threads;
+        for (; i < limit; i += n_threads) {
+            *partial_sum += 1.0 / (i * 4.0 + 1.0);
+            *partial_sum -= 1.0 / (i * 4.0 + 3.0);
         }
 
         if (stop_requested) {
@@ -133,8 +45,79 @@ int main(int argc, char **argv)
         }
     }
 
-    double pi = pi_sum * 4.0;
-    printf("\nPI approximation: %.15g\n", pi);
+    pthread_exit(partial_sum);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        fprintf(stderr, "Use: %s <number of threads>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    int num_threads = atoi(argv[1]);
+    if (num_threads <= 0) {
+        fprintf(stderr, "Number of threades need to be positive.\n");
+        return EXIT_FAILURE;
+    }
+
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_handler = sigint_handler;
+    sa.sa_flags = 0;
+
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        perror("sigaction");
+        return EXIT_FAILURE;
+    }
+
+    pthread_t *threads = malloc((size_t)num_threads * sizeof(pthread_t));
+    thread_data_t *thread_data = malloc((size_t)num_threads * sizeof(thread_data_t));
+
+    if (threads == NULL || thread_data == NULL) {
+        perror("malloc");
+        free(threads);
+        free(thread_data);
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 0; i < num_threads; ++i) {
+        thread_data[i].thread_id = i;
+        thread_data[i].num_threads = num_threads;
+
+        if (pthread_create(&threads[i], NULL, calc_pi, &thread_data[i]) != 0) {
+            perror("pthread_create");
+            stop_requested = 1;
+
+            for (int j = 0; j < i; ++j) {
+                void *status = NULL;
+                pthread_join(threads[j], &status);
+                free(status);
+            }
+
+            free(threads);
+            free(thread_data);
+            return EXIT_FAILURE;
+        }
+    }
+
+    double pi_sum = 0.0;
+
+    for (int i = 0; i < num_threads; ++i) {
+        void *status = NULL;
+        if (pthread_join(threads[i], &status) != 0) {
+            perror("pthread_join");
+            continue;
+        }
+
+        if (status != NULL) {
+            double *sum_from_thread = (double *)status;
+            pi_sum += *sum_from_thread;
+            free(sum_from_thread);
+        }
+    }
+
+    printf("\nPI approximation: %.15g\n", pi_sum * 4.0);
 
     free(threads);
     free(thread_data);
