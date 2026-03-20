374400ffe7cbf0c72264b448dd4a03469c326444
Anastasia Ryzhneva <a.ryzhneva@g.nsu.ru>
2026-03-20 10:27:56 +0700

    fix

--- 24216/a.ryzhneva/sem2-lab9/lab9.c
+++ 24216/a.ryzhneva/sem2-lab9/lab9.c
@@ -5,7 +5,8 @@
 #include <string.h>
 #include <signal.h>
 
-volatile sig_atomic_t stop_flag = 0;
+int stop_flag = 0;
+pthread_mutex_t flag_mutex = PTHREAD_MUTEX_INITIALIZER;
 int count_threads;
 
 typedef struct DataThread_t {
@@ -13,22 +14,38 @@ typedef struct DataThread_t {
     double value;
 } DataThread;
 
-void handle_sigint(int sig) {
+void* signal_thread_func(void* arg) {
+    sigset_t* set = (sigset_t*)arg;
+    int sig;
+    
+    if (sigwait(set, &sig) != 0) {
+        perror("sigwait failed");
+        pthread_exit(NULL);
+    }
+    
+    pthread_mutex_lock(&flag_mutex);
     stop_flag = 1;
+    pthread_mutex_unlock(&flag_mutex);
+    
+    return NULL;
 }
 
 void* thread_body(void* param) {
     DataThread* intermed_data = (DataThread*)param;
     double partial_sum = 0.0;
-    
     long long i = intermed_data->idx;
+    int local_stop = 0;
     
-    while (!stop_flag) {
+    while (!local_stop) {
         for (int k = 0; k < 1000000; k++) {
             partial_sum += 1.0 / (i * 4.0 + 1.0);
             partial_sum -= 1.0 / (i * 4.0 + 3.0);
             i += count_threads;
         }
+        
+        pthread_mutex_lock(&flag_mutex);
+        local_stop = stop_flag;
+        pthread_mutex_unlock(&flag_mutex);
     }
     
     for (int k = 0; k < 50000000; k++) {
@@ -64,22 +81,25 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    struct sigaction sa;
-    sa.sa_handler = handle_sigint;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    if (sigaction(SIGINT, &sa, NULL) == -1) {
-        perror("sigaction failed");
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGINT);
+    if (pthread_sigmask(SIG_BLOCK, &set, NULL) != 0) {
+        perror("pthread_sigmask failed");
         return EXIT_FAILURE;
     }
 
     pthread_t *threads = (pthread_t*)malloc(count_threads * sizeof(pthread_t));
     DataThread *intermed_data = (DataThread*)malloc(count_threads * sizeof(DataThread));
+    pthread_t sig_thread;
 
     if (!threads || !intermed_data) {
         fprintf(stderr, "Memory allocation failed\n");
         return EXIT_FAILURE;
     }
+
+    code = pthread_create(&sig_thread, NULL, signal_thread_func, (void*)&set);
+    check_code(code, argv[0], "creating signal thread");
 
     printf("Calculating Pi with %d threads. Press Ctrl+C to stop...\n", count_threads);
 
@@ -103,6 +123,7 @@ int main(int argc, char** argv) {
 
     free(threads);
     free(intermed_data);
+    pthread_mutex_destroy(&flag_mutex);
     
     return EXIT_SUCCESS;
 }
