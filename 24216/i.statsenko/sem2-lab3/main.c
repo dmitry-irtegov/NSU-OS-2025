33237984355764049e0e5ce46980d7994526390f
Ilya Statsenko <i.statsenko@g.nsu.ru>
2026-03-25 22:50:26 +0700

    fix

--- 24216/i.statsenko/sem2-lab3/main.c
+++ 24216/i.statsenko/sem2-lab3/main.c
@@ -3,48 +3,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NUM_THREADS 4
-#define STR_COUNT 3
-
-typedef struct {
-    const char **strs;
-    int count;
-} ThreadData;
-
-void *printer(void *arg) {
-    ThreadData *data = (ThreadData *)arg;
-    for (int i = 0; i < data->count; i++) {
-        fprintf(stderr, "%s\n", data->strs[i]);
+void *thread_print(void *data) {
+    char **strings = (char **)data;
+    for (int i = 0; strings[i] != NULL; i++) {
+        fprintf(stderr, "%s\n", strings[i]);
     }
     pthread_exit(NULL);
 }
 
 int main() {
-    const char *data[NUM_THREADS][STR_COUNT] = {
-        {"1 Thread, 1 string", "1 Thread, 2 string", "1 Thread, 3 string"},
-        {"2 Thread, 1 string", "2 Thread, 2 string", "2 Thread, 3 string"},
-        {"3 Thread, 1 string", "3 Thread, 2 string", "3 Thread, 3 string"},
-        {"4 Thread, 1 string", "4 Thread, 2 string", "4 Thread, 3 string"},
-    };
-
-    pthread_t threads[NUM_THREADS];
-    ThreadData thread_args[NUM_THREADS];
-
-    for (int i = 0; i < NUM_THREADS; i++) {
-        thread_args[i].strs = data[i];
-        thread_args[i].count = STR_COUNT;
-
-        int err = pthread_create(&threads[i], NULL, printer, &thread_args[i]);
+    pthread_t threads[4];
+    int err;
+    char *seq1[] = {"Thread 1: Hello", "Thread 1: from", "Thread 1: the first", "Thread 1: thread!", NULL};
+    char *seq2[] = {"Thread 2: Printing", "Thread 2: another", "Thread 2: sequence.", NULL};
+    char *seq3[] = {"Thread 3: Just", "Thread 3: two lines.", NULL};
+    char *seq4[] = {"Thread 4: One", "Thread 4: Two", "Thread 4: Three", "Thread 4: Four", "Thread 4: Five!", NULL};
+    char **sequences[4] = {seq1, seq2, seq3, seq4};
+    for (int i = 0; i < 4; i++) {
+        err = pthread_create(&threads[i], NULL, thread_print, sequences[i]);
         if (err != 0) {
-            fprintf(stderr, "Error creating thread %d: %s\n", i, strerror(err));
+            fprintf(stderr, "error create thread %d: %s\n", i, strerror(err));
             exit(EXIT_FAILURE);
         }
     }
-
-    for (int i = 0; i < NUM_THREADS; i++) {
-        int err = pthread_join(threads[i], NULL);
+    for (int i = 0; i < 4; i++) {
+        err = pthread_join(threads[i], NULL);
         if (err != 0) {
-            fprintf(stderr, "Error joining thread %d: %s\n", i, strerror(err));
+            fprintf(stderr, "error join thread %d: %s\n", i, strerror(err));
             exit(EXIT_FAILURE);
         }
     }
