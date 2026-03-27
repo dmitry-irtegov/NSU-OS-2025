3f6fe231688b013f709b57e8aae5e6bc52b35bac
StepanEfimov <s.efimov@g.nsu.ru>
2026-03-27 10:28:04 +0700

    Efimov-sem2-lab2

---
+++ 24216/s.efimov/sem2-lab2/lab2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+void print_line(char* line) {
+    for (int i = 0; i < 10; i++) {
+        fprintf(stderr, "%s %d\n", line, i);
+    }
+}
+void* thread_work(void* arg) {
+    print_line((char*)arg);
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t thread1;
+
+    if (pthread_create(&thread1, NULL, thread_work, "Thread 1") != 0) {
+        perror("Failed to create thread");
+        return EXIT_FAILURE;
+    }
+
+    if (pthread_join(thread1, NULL) != 0) {
+        perror("Failed to join thread");
+        return EXIT_FAILURE;
+    }
+
+    print_line("Main thread");
+
+    return EXIT_SUCCESS;
+}
