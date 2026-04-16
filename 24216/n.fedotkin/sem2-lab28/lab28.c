030c5f7c3b3e1da725a96486fa850ec1e6d1f245
Avtobot22 <n.fedotkin@g.nsu.ru>
2026-04-16 22:19:26 +0700

    fix lab28

--- 24216/n.fedotkin/sem2-lab28/lab28.c
+++ 24216/n.fedotkin/sem2-lab28/lab28.c
@@ -19,26 +19,33 @@ void reset_console() {
 void init_raw_mode() {
     tcgetattr(STDIN_FILENO, &orig_term_settings);
     atexit(reset_console);
-    
+
     struct termios raw_mod = orig_term_settings;
     raw_mod.c_lflag &= ~(ICANON | ECHO);
     raw_mod.c_cc[VMIN] = 1;
     raw_mod.c_cc[VTIME] = 0;
-    
+
     tcsetattr(STDIN_FILENO, TCSANOW, &raw_mod);
 }
 
 int parse_address(const char* url, char* host, char* port, char* path) {
     if (strstr(url, "http://") != url) return 0;
-    
+
     const char* base_url = url + 7;
     const char* slash_pos = strchr(base_url, '/');
-    char temp_host[512] = {0};
+    char temp_host[1024] = {0};
 
     if (slash_pos) {
-        strncpy(temp_host, base_url, slash_pos - base_url);
+        size_t host_len = slash_pos - base_url;
+        if (host_len >= sizeof(temp_host)) return 0;
+        strncpy(temp_host, base_url, host_len);
+
+        size_t path_len = strlen(slash_pos);
+        if (path_len >= 1024) return 0;
         strcpy(path, slash_pos);
     } else {
+        size_t host_len = strlen(base_url);
+        if (host_len >= sizeof(temp_host)) return 0;
         strcpy(temp_host, base_url);
         strcpy(path, "/");
     }
@@ -46,13 +53,16 @@ int parse_address(const char* url, char* host, char* port, char* path) {
     char* colon_pos = strchr(temp_host, ':');
     if (colon_pos) {
         *colon_pos = '\0';
+        if (strlen(temp_host) >= 256) return 0;
         strcpy(host, temp_host);
+        if (strlen(colon_pos + 1) >= 16) return 0;
         strcpy(port, colon_pos + 1);
     } else {
+        if (strlen(temp_host) >= 256) return 0;
         strcpy(host, temp_host);
         strcpy(port, "80");
     }
-    
+
     return 1;
 }
 
@@ -81,10 +91,10 @@ int connect_to_server(const char* host, const char* port) {
 void pause_for_user() {
     printf("\n[Press SPACE to continue...]\n");
     fflush(stdout);
-    
+
     fd_set kbd_fd;
     char key;
-    
+
     while (1) {
         FD_ZERO(&kbd_fd);
         FD_SET(STDIN_FILENO, &kbd_fd);
@@ -98,12 +108,14 @@ void pause_for_user() {
 }
 
 void fetch_and_display(int sock, const char* host, const char* path) {
-    char buffer[CHUNK_SIZE + 1];
+    char recv_buf[CHUNK_SIZE];
+    char line_buf[CHUNK_SIZE * 2];
+    int line_len = 0;
     char request[2048];
     int line_count = 0;
     int is_body = 0;
 
-    sprintf(request, "GET %s HTTP/1.0\r\nHost: %s\r\nConnection: close\r\n\r\n", path, host);
+    snprintf(request, sizeof(request), "GET %s HTTP/1.0\r\nHost: %s\r\nConnection: close\r\n\r\n", path, host);
     if (write(sock, request, strlen(request)) < 0) return;
 
     fd_set net_fd;
@@ -116,38 +128,35 @@ void fetch_and_display(int sock, const char* host, const char* path) {
         tv.tv_usec = 0;
 
         int status = select(sock + 1, &net_fd, NULL, NULL, &tv);
-        
+
         if (status <= 0) break;
 
         if (FD_ISSET(sock, &net_fd)) {
-            int bytes_received = recv(sock, buffer, CHUNK_SIZE, 0);
+            int bytes_received = recv(sock, recv_buf, sizeof(recv_buf), 0);
             if (bytes_received <= 0) break;
-            
-            buffer[bytes_received] = '\0';
-            char *ptr = buffer;
 
-            while (ptr && *ptr) {
-                char *newline = strchr(ptr, '\n');
-                if (newline) *newline = '\0';
+            for (int i = 0; i < bytes_received; i++) {
+                char c = recv_buf[i];
 
-                size_t len = strlen(ptr);
-                if (len > 0 && ptr[len - 1] == '\r') ptr[len - 1] = '\0';
-                len = strlen(ptr);
+                if (c == '\n') {
+                    if (line_len > 0 && line_buf[line_len - 1] == '\r') line_len--;
+                    line_buf[line_len] = '\0';
 
-                if (!is_body) {
-                    if (len == 0) is_body = 1;
-                } else {
-                    printf("%s\n", ptr);
-                    line_count++;
-                    
-                    if (line_count >= LINES_PER_PAGE) {
-                        pause_for_user();
-                        line_count = 0;
+                    if (!is_body) {
+                        if (line_len == 0) is_body = 1;
+                    } else {
+                        printf("%s\n", line_buf);
+                        line_count++;
+                        if (line_count >= LINES_PER_PAGE) {
+                            pause_for_user();
+                            line_count = 0;
+                        }
                     }
+                    line_len = 0;
+                } else {
+                    if (line_len < (int)sizeof(line_buf) - 1)
+                        line_buf[line_len++] = c;
                 }
-
-                if (newline) ptr = newline + 1;
-                else break;
             }
         }
     }
@@ -160,7 +169,7 @@ int main(int argc, char* argv[]) {
     }
 
     char host[256], port[16], path[1024];
-    
+
     if (!parse_address(argv[1], host, port, path)) {
         printf("Syntax: %s <URL> start with http://\n", argv[0]);
         return EXIT_FAILURE;
@@ -175,6 +184,8 @@ int main(int argc, char* argv[]) {
     }
 
     fetch_and_display(sock, host, path);
+
+    shutdown(sock, SHUT_RDWR);
     close(sock);
 
     return EXIT_SUCCESS;
