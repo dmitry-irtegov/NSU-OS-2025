38698c72ce4e2d65a320c3b949a5facf98e7e7b5
Гаев Владислав <90602583+WorstNormal@users.noreply.github.com>
2026-04-16 11:57:08 +0700

    Sem2 lab11 gaev (#815)
    * 123
    
    * 321
    
    * Delete 123.txt
    
    * Delete 321.txt
    
    * init
    
    * Delete 24216/v.gaev/sem2_lab1/lab11.c
    
    * init
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix logic
    
    * fix
    
    * fix
    
    * fix

---
+++ 24216/v.gaev/sem2_lab11/lab11.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sched.h>
+#include <stdatomic.h>
+
+pthread_mutex_t m[3];
+atomic_int child_ready = 0;
+
+void *child_task(void *arg) {
+    (void)arg;
+
+    pthread_mutex_lock(&m[2]);
+    atomic_store(&child_ready, 1);
+
+    for (int i = 0; i < 10; i++) {
+        pthread_mutex_lock(&m[i % 3]); 
+        fprintf(stderr, "Дочерняя нить: строка %d\n", i + 1);
+        pthread_mutex_unlock(&m[(i + 2) % 3]);
+    }
+    
+    pthread_mutex_unlock(&m[0]); 
+
+    return NULL;
+}
+
+int main(void) {
+    pthread_t thread;
+    pthread_mutexattr_t attr;
+
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
+
+    for (int i = 0; i < 3; i++) {
+        if (pthread_mutex_init(&m[i], &attr) != 0) {
+            perror("Ошибка инициализации мьютекса");
+            exit(EXIT_FAILURE);
+        }
+    }
+    pthread_mutexattr_destroy(&attr);
+
+    pthread_mutex_lock(&m[0]);
+
+    if (pthread_create(&thread, NULL, child_task, NULL) != 0) {
+        perror("Ошибка создания нити");
+        exit(EXIT_FAILURE);
+    }
+
+    while (!atomic_load(&child_ready)) {
+        sched_yield(); 
+    }
+    
+    for (int i = 0; i < 10; i++) {
+        pthread_mutex_lock(&m[(i + 1) % 3]);
+        fprintf(stderr, "Родительская нить: строка %d\n", i + 1);
+        pthread_mutex_unlock(&m[i % 3]); 
+    }
+    
+    pthread_mutex_unlock(&m[1]);
+
+    pthread_join(thread, NULL);
+
+    for (int i = 0; i < 3; i++) {
+        pthread_mutex_destroy(&m[i]);
+    }
+
+    fprintf(stderr, "Главная программа: все нити завершили работу.\n");
+
+    return 0;
+}
