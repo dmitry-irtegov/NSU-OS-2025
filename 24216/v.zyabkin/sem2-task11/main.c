1214e1939d6b5c6af9960a15d1a36301d9088079
Ziabkin Vladimir <v.zyabkin@g.nsu.ru>
2026-04-17 09:09:59 +0700

    fix solution

--- 24216/v.zyabkin/sem2-task11/main.c
+++ 24216/v.zyabkin/sem2-task11/main.c
@@ -1,11 +1,13 @@
 #include <pthread.h>
+#include <sched.h>
+#include <stdatomic.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct SafeSync {
-    pthread_mutex_t mutex_parent;
-    pthread_mutex_t mutex_child;
+    pthread_mutex_t m[3];
+    atomic_int child_start;
 } SyncContext;
 
 typedef struct ThreadArgs {
@@ -15,16 +17,27 @@ typedef struct ThreadArgs {
 } ThreadArgs;
 
 void thread_task(ThreadArgs* args) {
-    for (int i = 1; i <= 100; i++) {
+    if (!args->is_parent) {
+        pthread_mutex_lock(&args->sync->m[2]);
+        atomic_store(&args->sync->child_start, 1);
+    }
+
+    for (int i = 0; i < 100; i++) {
         if (args->is_parent) {
-            pthread_mutex_lock(&args->sync->mutex_parent);
-            fprintf(stderr, "%s%d\n", args->text, i);
-            pthread_mutex_unlock(&args->sync->mutex_child);
+            pthread_mutex_lock(&args->sync->m[(i + 1) % 3]);
+            fprintf(stderr, "%s%d\n", args->text, i + 1);
+            pthread_mutex_unlock(&args->sync->m[(i + 0) % 3]);
         } else {
-            pthread_mutex_lock(&args->sync->mutex_child);
-            fprintf(stderr, "%s%d\n", args->text, i);
-            pthread_mutex_unlock(&args->sync->mutex_parent);
+            pthread_mutex_lock(&args->sync->m[(i + 0) % 3]);
+            fprintf(stderr, "%s%d\n", args->text, i + 1);
+            pthread_mutex_unlock(&args->sync->m[(i + 2) % 3]);
         }
+    }
+
+    if (args->is_parent) {
+        pthread_mutex_unlock(&args->sync->m[(99 + 1) % 3]);
+    } else {
+        pthread_mutex_unlock(&args->sync->m[(99 + 0) % 3]);
     }
 }
 
@@ -35,16 +48,20 @@ void* launch_thread(void* arg) {
 
 int main() {
     SyncContext sync;
+    atomic_init(&sync.child_start, 0);
     pthread_mutexattr_t attr;
     
     pthread_mutexattr_init(&attr);
     pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
 
-    pthread_mutex_init(&sync.mutex_parent, &attr);
-    pthread_mutex_init(&sync.mutex_child, &attr);
+    for (int i = 0; i < 3; i++) {
+        if (pthread_mutex_init(&sync.m[i], &attr) != 0) {
+            fprintf(stderr, "Error initializing mutex %d\n", i);
+            exit(1);
+        }
+    }
 
-    pthread_mutex_lock(&sync.mutex_parent);
-    pthread_mutex_lock(&sync.mutex_child);
+    pthread_mutex_lock(&sync.m[0]);
 
     pthread_t thread_id;
 
@@ -52,7 +69,6 @@ int main() {
     ThreadArgs parent_args = { &sync, "Parent thread: string №", 1 };
 
     int result;
-
     result = pthread_create(&thread_id, NULL, launch_thread, &child_args);
 
     if (result != 0) {
@@ -60,16 +76,18 @@ int main() {
         exit(1);
     }
 
-    pthread_mutex_unlock(&sync.mutex_parent);
+    while (!atomic_load(&sync.child_start)) {
+        sched_yield();
+    }
 
     thread_task(&parent_args);
 
     pthread_join(thread_id, NULL);
 
-    pthread_mutex_destroy(&sync.mutex_parent);
-    pthread_mutex_destroy(&sync.mutex_child);
+    for (int i = 0; i < 3; i++) {
+        pthread_mutex_destroy(&sync.m[i]);
+    }
     pthread_mutexattr_destroy(&attr);
     
     return 0;
 }
-
