d6c447a88d5509725144766d779b0aaa9e27e9d6
digitalunites <pr.krasnianski@gmail.com>
2026-04-21 12:55:16 +0700

    NORMAL

--- 24214/p.krasnyanskii/sem2-lab11/11.c
+++ 24214/p.krasnyanskii/sem2-lab11/11.c
@@ -19,8 +19,7 @@ void* child_thread(void* arg) {
 int main(void) {
     pthread_mutexattr_t attr;
     pthread_mutexattr_init(&attr);
-    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
-
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_NORMAL);
     pthread_mutex_init(&mutex_parent, &attr);
     pthread_mutex_init(&mutex_child,  &attr);
     pthread_mutexattr_destroy(&attr);
