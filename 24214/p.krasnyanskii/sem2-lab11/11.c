aeadc533d62596203e592bb450301dbd9d2be090
digitalunites <pr.krasnianski@gmail.com>
2026-04-21 13:31:52 +0700

    Condvar solution

--- 24214/p.krasnyanskii/sem2-lab11/11.c
+++ 24214/p.krasnyanskii/sem2-lab11/11.c
@@ -2,49 +2,40 @@
 #include <pthread.h>
 #include <unistd.h>
 
-static pthread_mutex_t mutex_parent;
-static pthread_mutex_t mutex_child;
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t  cond = PTHREAD_COND_INITIALIZER;
+int turn = 0;
 
 void* child_thread(void* arg) {
     for (int i = 0; i < 10; i++) {
-        pthread_mutex_lock(&mutex_child);
-
+        pthread_mutex_lock(&mtx);
+        while (turn != 1)
+            pthread_cond_wait(&cond, &mtx);
         printf("Child thread: %d\n", i + 1);
-
-        pthread_mutex_unlock(&mutex_parent);
+        turn = 0;
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&mtx);
+        usleep(100000);
     }
     return NULL;
 }
 
 int main(void) {
-    pthread_mutexattr_t attr;
-    pthread_mutexattr_init(&attr);
-    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_NORMAL);
-    pthread_mutex_init(&mutex_parent, &attr);
-    pthread_mutex_init(&mutex_child,  &attr);
-    pthread_mutexattr_destroy(&attr);
-
-    pthread_mutex_lock(&mutex_child);
-
     pthread_t tid;
-    if (pthread_create(&tid, NULL, child_thread, NULL) != 0) {
-        perror("pthread_create");
-        return 1;
-    }
+    pthread_create(&tid, NULL, child_thread, NULL);
 
     for (int i = 0; i < 10; i++) {
-        pthread_mutex_lock(&mutex_parent);
-
+        pthread_mutex_lock(&mtx);
+        while (turn != 0)
+            pthread_cond_wait(&cond, &mtx);
         printf("Parent thread: %d\n", i + 1);
-
-        pthread_mutex_unlock(&mutex_child);
+        turn = 1;
+        pthread_cond_signal(&cond);
+        pthread_mutex_unlock(&mtx);
+        usleep(100000);
     }
 
     pthread_join(tid, NULL);
-
-    pthread_mutex_destroy(&mutex_parent);
-    pthread_mutex_destroy(&mutex_child);
-
     return 0;
 }
 
