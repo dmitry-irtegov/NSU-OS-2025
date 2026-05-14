21348575cf3ae2d96ca5babc6fc5469a1ac889f4
aryzhneva <a.ryzhneva@g.nsu.ru>
2026-05-14 18:53:50 +0700

    add parse domain

--- 24216/a.ryzhneva/sem2-lab32/lab32.c
+++ 24216/a.ryzhneva/sem2-lab32/lab32.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/socket.h>
@@ -33,7 +34,8 @@ typedef struct CacheEntry {
 
 CacheEntry *cache_head = NULL;
 pthread_mutex_t cache_list_mutex = PTHREAD_MUTEX_INITIALIZER;
-struct addrinfo *target_addrinfo = NULL;
+static const char *default_target_host = NULL;
+static const char *default_target_port = NULL;
 
 void sig_handler(int signum) {
     if (signum == SIGINT || signum == SIGTERM) {
@@ -179,16 +181,116 @@ int parse_request(int client_fd, char *req_buf, size_t req_buf_size, int *req_le
     return 0;
 }
 
-void resolve_target(const char *target_host, const char *target_port) {
+int parse_target_from_uri(const char *uri, char *host, size_t host_len, char *port, size_t port_len) {
+    const char *scheme = "http://";
+    size_t scheme_len = strlen(scheme);
+
+    if (strncmp(uri, scheme, scheme_len) != 0) {
+        return 0;
+    }
+
+    const char *host_start = uri + scheme_len;
+    const char *path_start = strchr(host_start, '/');
+    const char *host_end = path_start ? path_start : uri + strlen(uri);
+    const char *colon = memchr(host_start, ':', (size_t)(host_end - host_start));
+
+    if (host_start == host_end) {
+        return -1;
+    }
+
+    if (colon) {
+        size_t host_part_len = (size_t)(colon - host_start);
+        size_t port_part_len = (size_t)(host_end - colon - 1);
+
+        if (host_part_len == 0 || port_part_len == 0) {
+            return -1;
+        }
+
+        if (host_part_len >= host_len || port_part_len >= port_len) {
+            return -1;
+        }
+
+        memcpy(host, host_start, host_part_len);
+        host[host_part_len] = '\0';
+
+        memcpy(port, colon + 1, port_part_len);
+        port[port_part_len] = '\0';
+    } else {
+        size_t host_part_len = (size_t)(host_end - host_start);
+        if (host_part_len == 0 || host_part_len >= host_len) {
+            return -1;
+        }
+
+        memcpy(host, host_start, host_part_len);
+        host[host_part_len] = '\0';
+
+        if (snprintf(port, port_len, "%s", "80") >= (int)port_len) {
+            return -1;
+        }
+    }
+
+    return 1;
+}
+
+int parse_host_header(const char *request, char *host, size_t host_len, char *port, size_t port_len) {
+    const char *p = request;
+    while ((p = strstr(p, "\r\n")) != NULL) {
+        p += 2;
+        if (strncasecmp(p, "Host:", 5) == 0) {
+            p += 5;
+            while (*p == ' ' || *p == '\t') {
+                p++;
+            }
+            const char *line_end = strstr(p, "\r\n");
+            if (!line_end) {
+                return -1;
+            }
+
+            const char *colon = memchr(p, ':', (size_t)(line_end - p));
+            if (colon) {
+                size_t host_len_part = (size_t)(colon - p);
+                size_t port_len_part = (size_t)(line_end - colon - 1);
+                if (host_len_part == 0 || port_len_part == 0) {
+                    return -1;
+                }
+                if (host_len_part >= host_len || port_len_part >= port_len) {
+                    return -1;
+                }
+
+                memcpy(host, p, host_len_part);
+                host[host_len_part] = '\0';
+                memcpy(port, colon + 1, port_len_part);
+                port[port_len_part] = '\0';
+            } else {
+                size_t host_len_part = (size_t)(line_end - p);
+                if (host_len_part == 0 || host_len_part >= host_len) {
+                    return -1;
+                }
+
+                memcpy(host, p, host_len_part);
+                host[host_len_part] = '\0';
+                if (snprintf(port, port_len, "%s", "80") >= (int)port_len) {
+                    return -1;
+                }
+            }
+            return 1;
+        }
+    }
+    return 0;
+}
+
+struct addrinfo *resolve_target(const char *target_host, const char *target_port) {
     struct addrinfo hints;
+    struct addrinfo *result = NULL;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
 
-    if (getaddrinfo(target_host, target_port, &hints, &target_addrinfo) != 0) {
-        perror("Ошибка getaddrinfo для целевого узла");
-        exit(EXIT_FAILURE);
+    if (getaddrinfo(target_host, target_port, &hints, &result) != 0) {
+        return NULL;
     }
+
+    return result;
 }
 
 int init_listen_socket(const char *port) {
@@ -253,8 +355,49 @@ void* handle_client(void* arg) {
         return NULL;
     }
 
+    char target_host[256];
+    char target_port[16];
+    int parsed = parse_target_from_uri(uri, target_host, sizeof(target_host), target_port, sizeof(target_port));
+    int uri_is_absolute = (parsed == 1);
+    if (parsed == -1) {
+        close_connection(client_fd);
+        free(arg);
+        return NULL;
+    } else if (parsed == 0) {
+        parsed = parse_host_header(req_buf, target_host, sizeof(target_host), target_port, sizeof(target_port));
+        if (parsed == -1) {
+            close_connection(client_fd);
+            free(arg);
+            return NULL;
+        }
+    }
+
+    const char *host_to_use = (parsed == 1) ? target_host : default_target_host;
+    const char *port_to_use = (parsed == 1) ? target_port : default_target_port;
+    if (!host_to_use || !port_to_use) {
+        close_connection(client_fd);
+        free(arg);
+        return NULL;
+    }
+
+    char cache_key[MAX_URI_LEN + 300];
+    if (uri_is_absolute) {
+        if (snprintf(cache_key, sizeof(cache_key), "%s", uri) >= (int)sizeof(cache_key)) {
+            close_connection(client_fd);
+            free(arg);
+            return NULL;
+        }
+    } else {
+        const char *path_prefix = (uri[0] == '/') ? "" : "/";
+        if (snprintf(cache_key, sizeof(cache_key), "http://%s:%s%s%s", host_to_use, port_to_use, path_prefix, uri) >= (int)sizeof(cache_key)) {
+            close_connection(client_fd);
+            free(arg);
+            return NULL;
+        }
+    }
+
     pthread_mutex_lock(&cache_list_mutex);
-    CacheEntry *entry = cache_lookup(uri);
+    CacheEntry *entry = cache_lookup(cache_key);
     int is_writer = 0;
 
     if (entry != NULL) {
@@ -262,7 +405,7 @@ void* handle_client(void* arg) {
         entry->reference_count++;
         pthread_mutex_unlock(&entry->mutex);
     } else {
-        entry = cache_add_entry(uri);
+        entry = cache_add_entry(cache_key);
         if (!entry) {
             pthread_mutex_unlock(&cache_list_mutex);
             close_connection(client_fd);
@@ -274,8 +417,12 @@ void* handle_client(void* arg) {
     pthread_mutex_unlock(&cache_list_mutex);
 
     if (is_writer) {
-        int server_fd = socket(target_addrinfo->ai_family, target_addrinfo->ai_socktype, target_addrinfo->ai_protocol);
-        
+        struct addrinfo *target_addrinfo = resolve_target(host_to_use, port_to_use);
+        int server_fd = -1;
+        if (target_addrinfo) {
+            server_fd = socket(target_addrinfo->ai_family, target_addrinfo->ai_socktype, target_addrinfo->ai_protocol);
+        }
+
         if (server_fd >= 0 && connect(server_fd, target_addrinfo->ai_addr, target_addrinfo->ai_addrlen) == 0) {
 
             if (send_all(server_fd, req_buf, req_len) < 0) {
@@ -328,6 +475,10 @@ void* handle_client(void* arg) {
             }
             const char *err_502 = "HTTP/1.0 502 Bad Gateway\r\n\r\n";
             send_all(client_fd, err_502, strlen(err_502));
+        }
+
+        if (target_addrinfo) {
+            freeaddrinfo(target_addrinfo);
         }
 
         pthread_mutex_lock(&entry->mutex);
@@ -388,7 +539,8 @@ int main(int argc, char** argv) {
     sigaction(SIGINT, &sa, NULL);
     sigaction(SIGTERM, &sa, NULL);
 
-    resolve_target(argv[2], argv[3]);
+    default_target_host = argv[2];
+    default_target_port = argv[3];
     int listen_fd = init_listen_socket(argv[1]);
 
     while(server_running) {
@@ -423,9 +575,6 @@ int main(int argc, char** argv) {
         pthread_attr_destroy(&attr);
     }
 
-    if (target_addrinfo) {
-        freeaddrinfo(target_addrinfo);
-    }
     close_connection(listen_fd);
     cache_free_all();
 
