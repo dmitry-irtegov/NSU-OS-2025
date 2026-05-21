ea0dc7a1d4fc63c0d7a98edad0fd18e22260bd2d
Ivan Kiryushin <97592489+qzzkq@users.noreply.github.com>
2026-05-21 12:10:39 +0700

    Sem2 lab2 Kiryushin 24215 (#686)
    * sem2-lab1
    
    * sem2-lab1
    
    * sem2-lab2

---
+++ 24215/i.kiryushin/sem2-lab2/lab2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+
+
+void* thread_func(void* param) {
+    for (int i = 0; i< 10; i++) {
+        printf("good\n");
+    }
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t thread;
+    
+    int result = pthread_create(&thread, NULL, thread_func, NULL);
+    if (result != 0){
+        char buf[256];
+        strerror_r(result, buf, sizeof(buf));
+        fprintf(stderr, "error creating thread: %s\n", buf);
+        exit(EXIT_FAILURE);
+    }
+    int pthread_result = pthread_join(thread, NULL);
+    if (pthread_result != 0) {
+        char buf[256];
+        strerror_r(pthread_result, buf, sizeof(buf));
+        fprintf(stderr, "error joining thread: %s\n", buf);
+        exit(EXIT_FAILURE);
+    }
+    
+    for (int i = 0; i < 10; i++) {
+        printf("excellent\n");
+    }
+    pthread_exit(NULL);
+
+}
