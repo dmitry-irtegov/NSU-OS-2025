34b5a64db48b473abe53d5b688e2e9a8ca6a5868
Egor Dashkovskii <e.dashkovskii1@g.nsu.ru>
2026-05-22 07:40:46 +0700

    proxy

--- 24216/e.dashkovskii1/sem2-lab31/main.go
+++ 24216/e.dashkovskii1/sem2-lab31/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net"
 	"net/url"
@@ -55,11 +56,65 @@ var (
 	upmap = make(map[int]*Conn)
 )
 
+func readEINTR(fd int, buf []byte) (int, error) {
+	for {
+		n, err := unix.Read(fd, buf)
+		if errors.Is(err, unix.EINTR) {
+			continue
+		}
+		return n, err
+	}
+}
+
+func writeEINTR(fd int, buf []byte) (int, error) {
+	for {
+		n, err := unix.Write(fd, buf)
+		if errors.Is(err, unix.EINTR) {
+			continue
+		}
+		return n, err
+	}
+}
+
+func acceptEINTR(fd int) (int, unix.Sockaddr, error) {
+	for {
+		nfd, sa, err := unix.Accept(fd)
+		if errors.Is(err, unix.EINTR) {
+			continue
+		}
+		return nfd, sa, err
+	}
+}
+
+func pollEINTR(fds []unix.PollFd, timeoutMs int) (int, error) {
+	for {
+		n, err := unix.Poll(fds, timeoutMs)
+		if errors.Is(err, unix.EINTR) {
+			continue
+		}
+		return n, err
+	}
+}
+
+func connectEINTR(fd int, sa unix.Sockaddr) error {
+	for {
+		err := unix.Connect(fd, sa)
+		if errors.Is(err, unix.EINTR) {
+			continue
+		}
+		if errors.Is(err, unix.EINPROGRESS) {
+			return nil
+		}
+		return err
+	}
+}
+
 func makeListener(port int) (int, error) {
 	fd, err := unix.Socket(unix.AF_INET, unix.SOCK_STREAM, 0)
 	if err != nil {
 		return -1, fmt.Errorf("socket failed")
 	}
+	unix.SetNonblock(fd, true)
 	if err := unix.SetsockoptInt(fd, unix.SOL_SOCKET, unix.SO_REUSEADDR, 1); err != nil {
 		return -1, fmt.Errorf("socketopt failed")
 	}
@@ -73,7 +128,7 @@ func makeListener(port int) (int, error) {
 }
 
 func parseUrl(reqLine string) (string, string, *url.URL, error) {
-	s := strings.Split(reqLine, " ")
+	s := strings.Fields(reqLine)
 	if len(s) < 3 {
 		return "", "", nil, fmt.Errorf("split failed")
 	}
@@ -88,13 +143,19 @@ func parseUrl(reqLine string) (string, string, *url.URL, error) {
 
 func connectUpstream(host string, port int) (int, error) {
 	ips, err := net.LookupHost(host)
-	if err != nil {
+	if err != nil || len(ips) == 0 {
 		return -1, fmt.Errorf("dns resolve failed")
 	}
 
-	ip := net.ParseIP(ips[0]).To4()
+	var ip net.IP
+	for _, ipStr := range ips {
+		if parsed := net.ParseIP(ipStr).To4(); parsed != nil {
+			ip = parsed
+			break
+		}
+	}
 	if ip == nil {
-		return -1, fmt.Errorf("parseIP failed")
+		return -1, fmt.Errorf("no ipv4 address found")
 	}
 
 	var addr [4]byte
@@ -104,8 +165,9 @@ func connectUpstream(host string, port int) (int, error) {
 	if err != nil {
 		return -1, fmt.Errorf("failed socket")
 	}
+	unix.SetNonblock(fd, true)
 
-	if err := unix.Connect(fd, &unix.SockaddrInet4{Port: port, Addr: addr}); err != nil {
+	if err := connectEINTR(fd, &unix.SockaddrInet4{Port: port, Addr: addr}); err != nil {
 		unix.Close(fd)
 		return -1, fmt.Errorf("failed connect")
 	}
@@ -113,10 +175,11 @@ func connectUpstream(host string, port int) (int, error) {
 }
 
 func handleNewClient(serverFd int) error {
-	cfd, _, err := unix.Accept(serverFd)
+	cfd, _, err := acceptEINTR(serverFd)
 	if err != nil {
 		return fmt.Errorf("accept failed")
 	}
+	unix.SetNonblock(cfd, false)
 
 	if len(conns) >= MaxConns {
 		unix.Close(cfd)
@@ -143,18 +206,27 @@ func closeConn(clientFd int) {
 
 func handleClientReadable(c *Conn) {
 	buf := make([]byte, BufSize)
-	n, err := unix.Read(c.ClientFd, buf)
-	if err != nil || n <= 0 {
+	n, err := readEINTR(c.ClientFd, buf)
+	if err != nil {
+		c.State = StateDone
+		return
+	}
+	if n == 0 {
 		c.State = StateDone
 		return
 	}
 	c.Req = append(c.Req, buf[:n]...)
+	if len(c.Req) > 32768 {
+		c.State = StateDone
+		return
+	}
 
 	if !bytes.Contains(c.Req, []byte("\r\n\r\n")) {
 		return
 	}
 
-	firstLine := strings.SplitN(string(c.Req), "\r\n", 2)[0]
+	idx := bytes.Index(c.Req, []byte("\r\n"))
+	firstLine := string(c.Req[:idx])
 	method, _, parsedUrl, err := parseUrl(firstLine)
 	if err != nil {
 		c.State = StateDone
@@ -165,11 +237,15 @@ func handleClientReadable(c *Conn) {
 		return
 	}
 	host := parsedUrl.Hostname()
+	if host == "" {
+		c.State = StateDone
+		return
+	}
 	port := parsedUrl.Port()
 	if port == "" {
 		port = "80"
 	}
-	cacheKey := fmt.Sprintf("%s:%s%s", host, port, parsedUrl.Path)
+	cacheKey := fmt.Sprintf("%s:%s%s", host, port, parsedUrl.RequestURI())
 
 	if ce, ok := cache[cacheKey]; ok {
 		c.SendData = ce.Data
@@ -177,7 +253,13 @@ func handleClientReadable(c *Conn) {
 		c.State = StateSendCache
 		return
 	}
-	portInt, _ := strconv.Atoi(port)
+
+	portInt, err := strconv.Atoi(port)
+	if err != nil {
+		c.State = StateDone
+		return
+	}
+
 	ufd, err := connectUpstream(host, portInt)
 	if err != nil {
 		c.State = StateDone
@@ -195,24 +277,50 @@ func handleClientReadable(c *Conn) {
 	c.State = StateSendUpstreamReq
 }
 
-func handleUpstreamWritable(c *Conn) {
-	if c.UpReqPtr >= len(c.UpReq) {
-		c.State = StateForward
-		return
+func isCacheable(resp []byte) bool {
+	headerEnd := bytes.Index(resp, []byte("\r\n\r\n"))
+	if headerEnd < 0 {
+		return false
 	}
-	n, err := unix.Write(c.UpstreamFd, c.UpReq[c.UpReqPtr:])
-	if n <= 0 || err != nil {
-		c.State = StateDone
-		return
+	header := resp[:headerEnd]
+	body := resp[headerEnd+4:]
+
+	statusEnd := bytes.Index(header, []byte("\r\n"))
+	if statusEnd < 0 {
+		statusEnd = len(header)
 	}
-	c.UpReqPtr += n
-	if c.UpReqPtr >= len(c.UpReq) {
-		c.State = StateForward
+	statusLine := string(header[:statusEnd])
+	parts := strings.Fields(statusLine)
+	if len(parts) < 2 || parts[1] != "200" {
+		return false
 	}
+
+	lines := bytes.Split(header, []byte("\r\n"))
+	for _, ln := range lines {
+		i := bytes.IndexByte(ln, ':')
+		if i < 0 {
+			continue
+		}
+		name := strings.ToLower(strings.TrimSpace(string(ln[:i])))
+		val := strings.TrimSpace(string(ln[i+1:]))
+		if name == "transfer-encoding" && strings.EqualFold(val, "chunked") {
+			return false
+		}
+		if name == "content-length" {
+			cl, err := strconv.Atoi(val)
+			if err != nil || cl < 0 {
+				return false
+			}
+			if len(body) != cl {
+				return false
+			}
+		}
+	}
+	return true
 }
 
-func upstreamFinish(c *Conn) {
-	if len(c.RespBuf) > 0 {
+func upstreamFinish(c *Conn, success bool) {
+	if success && len(c.RespBuf) > 0 && isCacheable(c.RespBuf) {
 		cache[c.CacheKey] = &CacheEntry{Data: c.RespBuf}
 	}
 	if c.UpstreamFd >= 0 {
@@ -228,20 +336,56 @@ func upstreamFinish(c *Conn) {
 
 func handleUpstreamReadable(c *Conn) {
 	buf := make([]byte, BufSize)
-	n, err := unix.Read(c.UpstreamFd, buf)
-	if n <= 0 || err != nil {
-		upstreamFinish(c)
+	n, err := readEINTR(c.UpstreamFd, buf)
+	if err != nil {
+		upstreamFinish(c, false)
+		return
+	}
+	if n == 0 {
+		upstreamFinish(c, true)
 		return
 	}
 	c.RespBuf = append(c.RespBuf, buf[:n]...)
 }
 
-func handleClientWritableForward(c *Conn) {
-	if c.ClientWritePtr >= len(c.RespBuf) {
+func handleUpstreamWritable(c *Conn) {
+	if c.UpReqPtr == 0 && c.UpstreamFd >= 0 {
+		soerr, err := unix.GetsockoptInt(c.UpstreamFd, unix.SOL_SOCKET, unix.SO_ERROR)
+		if err != nil {
+			upstreamFinish(c, false)
+			return
+		}
+		if soerr != 0 {
+			upstreamFinish(c, false)
+			return
+		}
+		_ = unix.SetNonblock(c.UpstreamFd, false)
+	}
+
+	if c.UpReqPtr >= len(c.UpReq) {
+		c.State = StateForward
 		return
 	}
-	n, err := unix.Write(c.ClientFd, c.RespBuf[c.ClientWritePtr:])
-	if n <= 0 || err != nil {
+	n, err := writeEINTR(c.UpstreamFd, c.UpReq[c.UpReqPtr:])
+	if err != nil || n <= 0 {
+		c.State = StateDone
+		return
+	}
+	c.UpReqPtr += n
+	if c.UpReqPtr >= len(c.UpReq) {
+		c.State = StateForward
+	}
+}
+
+func handleClientWritableForward(c *Conn) {
+	if c.ClientWritePtr >= len(c.RespBuf) {
+		if c.UpstreamEOF {
+			c.State = StateDone
+		}
+		return
+	}
+	n, err := writeEINTR(c.ClientFd, c.RespBuf[c.ClientWritePtr:])
+	if err != nil || n <= 0 {
 		c.State = StateDone
 		return
 	}
@@ -252,8 +396,8 @@ func handleClientWritableForward(c *Conn) {
 }
 
 func handleSendCache(c *Conn) {
-	n, err := unix.Write(c.ClientFd, c.SendData[c.SendPtr:])
-	if n <= 0 || err != nil {
+	n, err := writeEINTR(c.ClientFd, c.SendData[c.SendPtr:])
+	if err != nil || n <= 0 {
 		c.State = StateDone
 		return
 	}
@@ -309,9 +453,10 @@ func main() {
 			}
 		}
 
-		_, err := unix.Poll(fds, 1000)
+		_, err := pollEINTR(fds, 1000)
 		if err != nil {
-			continue
+			fmt.Fprintln(os.Stderr, "poll:", err)
+			return
 		}
 
 		if fds[0].Revents&unix.POLLIN != 0 {
@@ -324,12 +469,9 @@ func main() {
 			}
 			fd := int(fds[i].Fd)
 			revents := fds[i].Revents
+
 			if c, ok := conns[fd]; ok {
-				if revents&(unix.POLLERR|unix.POLLNVAL) != 0 {
-					c.State = StateDone
-					continue
-				}
-				if revents&(unix.POLLIN|unix.POLLHUP) != 0 && c.State == StateReadRequest {
+				if revents&unix.POLLIN != 0 && c.State == StateReadRequest {
 					handleClientReadable(c)
 				}
 				if revents&unix.POLLOUT != 0 {
@@ -340,16 +482,20 @@ func main() {
 						handleSendCache(c)
 					}
 				}
-			} else if c, ok := upmap[fd]; ok {
-				if revents&(unix.POLLERR|unix.POLLNVAL) != 0 {
-					upstreamFinish(c)
-					continue
+				if revents&(unix.POLLERR|unix.POLLNVAL|unix.POLLHUP) != 0 {
+					c.State = StateDone
 				}
-				if revents&unix.POLLOUT != 0 && c.State == StateSendUpstreamReq {
+			} else if c, ok := upmap[fd]; ok {
+				if revents&unix.POLLIN != 0 {
+					handleUpstreamReadable(c)
+				}
+				if c.UpstreamFd == fd && revents&unix.POLLOUT != 0 {
 					handleUpstreamWritable(c)
 				}
-				if revents&(unix.POLLIN|unix.POLLHUP) != 0 && c.State == StateForward {
-					handleUpstreamReadable(c)
+				if c.UpstreamFd == fd && revents&(unix.POLLERR|unix.POLLHUP|unix.POLLNVAL) != 0 {
+					if !c.UpstreamEOF {
+						upstreamFinish(c, false)
+					}
 				}
 			}
 		}
