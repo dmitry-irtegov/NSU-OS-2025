b6a9efaa7f2f2129db24be2be9b3f98bf47d81e3
Ivan Badin <Streafyhs@gmail.com>
2026-05-29 12:29:44 +0700

    i.badin sem2-lab1 (#812)
    * sem2-lab1
    
    * Убрал буфферизацию для stdout

---
+++ 23214/i.badin/sem2-lab1/lab1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+
+void *thread_function(void *arg) {
+    for (int i = 0; i < 10; ++i) {
+        printf("child %d\n", i + 1);
+    }
+    return NULL;
+}
+
+int main() {
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    pthread_t thread;
+    int errnum;
+
+    errnum = pthread_create(&thread, NULL, thread_function, NULL);
+    if (errnum != 0) {
+        fprintf(stderr, "error while creating thread: %s\n", strerror(errnum));
+        return 1;
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        printf("parent %d\n", i + 1);
+    }
+
+    pthread_exit(0);
+}
